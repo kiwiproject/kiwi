9548fb0aa40cca2d015be1cc54b4e2911ad8e0c8
Scott Leberknight <scottleber@gmail.com>
2020-03-10 15:22:38 -0400

    Update UncheckedInterruptedException to latest code
    * Update UncheckedInterruptedException and test
    * Fix Sonar warning re: needing to add synchronized to getCause()
    * Changed DefaultKiwiEnvironmentTest and UncheckedInterruptedExceptionTest test to JUnit Jupiter
    
    Fixes #7

--- src/main/java/org/kiwiproject/base/UncheckedInterruptedException.java
+++ src/main/java/org/kiwiproject/base/UncheckedInterruptedException.java
@@ -2,18 +2,40 @@ package org.kiwiproject.base;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Wraps an {@link InterruptedException} with an unchecked exception. Basically, copied Java's
+ * {@link java.io.UncheckedIOException}, renamed it, and modified it.
+ */
 public class UncheckedInterruptedException extends RuntimeException {
 
+    /**
+     * Constructs an instance of this class.
+     *
+     * @param message the detail message, can be null
+     * @param cause   the {@code InterruptedException}
+     * @throws NullPointerException if the cause is {@code null}
+     */
     public UncheckedInterruptedException(String message, InterruptedException cause) {
         super(message, requireNonNull(cause, "cause cannot be null"));
     }
 
+    /**
+     * Constructs an instance of this class.
+     *
+     * @param cause the {@code InterruptedException}
+     * @throws NullPointerException if the cause is {@code null}
+     */
     public UncheckedInterruptedException(InterruptedException cause) {
         super(requireNonNull(cause, "cause cannot be null"));
     }
 
+    /**
+     * Returns the cause of this exception.
+     *
+     * @return the {@link InterruptedException} which is the cause of this exception
+     */
     @Override
-    public InterruptedException getCause() {
+    public synchronized InterruptedException getCause() {
         return (InterruptedException) super.getCause();
     }
 }
--- src/test/java/org/kiwiproject/base/UncheckedInterruptedExceptionTest.java
+++ src/test/java/org/kiwiproject/base/UncheckedInterruptedExceptionTest.java
@@ -1,15 +1,40 @@
 package org.kiwiproject.base;
 
-import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class UncheckedInterruptedExceptionTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-    private InterruptedException cause = new InterruptedException("Interrupted!");
+@DisplayName("UncheckedInterruptedException")
+class UncheckedInterruptedExceptionTest {
+
+    private InterruptedException cause;
+
+    @BeforeEach
+    void setUp() {
+        cause = new InterruptedException("Interrupted!");
+    }
+
+    @SuppressWarnings("ThrowableNotThrown")
+    @Test
+    void testConstructWithMessageButNoCause_ThrowsNPE() {
+        assertThatThrownBy(() -> new UncheckedInterruptedException("message", null))
+                .isExactlyInstanceOf(NullPointerException.class)
+                .hasMessage("cause cannot be null");
+    }
+
+    @SuppressWarnings("ThrowableNotThrown")
+    @Test
+    void testConstructWithNoCause_ThrowsNPE() {
+        assertThatThrownBy(() -> new UncheckedInterruptedException(null))
+                .isExactlyInstanceOf(NullPointerException.class)
+                .hasMessage("cause cannot be null");
+    }
 
     @Test
-    public void testConstruct_WithMessageAndCause() {
+    void testConstruct_WithMessageAndCause() {
         String message = "Interrupted while resting...";
         UncheckedInterruptedException ex = new UncheckedInterruptedException(message, cause);
         assertThat(ex.getMessage()).isEqualTo(message);
@@ -17,7 +42,7 @@ public class UncheckedInterruptedExceptionTest {
     }
 
     @Test
-    public void testConstruct_WithOnlyCause() {
+    void testConstruct_WithOnlyCause() {
         UncheckedInterruptedException ex = new UncheckedInterruptedException(cause);
         assertThat(ex.getMessage()).contains(cause.getMessage());
         assertThat(ex.getCause()).isEqualTo(cause);
