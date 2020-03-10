de239d3b0276aa90999922150237034d9007be17
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-03-10 15:29:53 -0400

    Update UncheckedInterruptedException to latest code (#17)
    * Update UncheckedInterruptedException and test
    * Fix Sonar warning re: needing to add synchronized to getCause()
    * Changed DefaultKiwiEnvironmentTest and UncheckedInterruptedExceptionTest test to JUnit Jupiter
    
    Fixes #7

--- src/test/java/org/kiwiproject/base/DefaultKiwiEnvironmentTest.java
+++ src/test/java/org/kiwiproject/base/DefaultKiwiEnvironmentTest.java
@@ -2,8 +2,9 @@ package org.kiwiproject.base;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.kiwiproject.collect.KiwiMaps;
 
 import java.sql.Time;
@@ -23,26 +24,27 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class DefaultKiwiEnvironmentTest {
+@DisplayName("DefaultKiwiEnvironment")
+class DefaultKiwiEnvironmentTest {
 
     private static final long DEFAULT_DELTA_MILLIS = 50;
 
     private DefaultKiwiEnvironment env;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         env = new DefaultKiwiEnvironment();
     }
 
     @Test
-    public void testCurrentDate() {
+    void testCurrentDate() {
         long now = new Date().getTime();
         long envNow = env.currentDate().getTime();
         assertNow(envNow, now);
     }
 
     @Test
-    public void testCurrentTime() {
+    void testCurrentTime() {
         long millis = System.currentTimeMillis();
         long now = new Time(millis).getTime();
         long envTime = env.currentTime().getTime();
@@ -50,7 +52,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentTimestamp() {
+    void testCurrentTimestamp() {
         long millis = System.currentTimeMillis();
         long now = new Timestamp(millis).getTime();
         long envTime = env.currentTimestamp().getTime();
@@ -58,14 +60,14 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentInstant_InDefaultTimeZone() {
+    void testCurrentInstant_InDefaultTimeZone() {
         long now = Instant.now().toEpochMilli();
         long envTime = env.currentInstant().toEpochMilli();
         assertNow(envTime, now);
     }
 
     @Test
-    public void testCurrentInstant_ForAllAvailableZoneIds() {
+    void testCurrentInstant_ForAllAvailableZoneIds() {
         Set<String> zoneIds = ZoneId.getAvailableZoneIds();
         zoneIds.forEach(zone -> {
             ZoneId zoneId = ZoneId.of(zone);
@@ -76,7 +78,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentLocalDate_InDefaultTimeZone() {
+    void testCurrentLocalDate_InDefaultTimeZone() {
         ZoneId defaultZone = ZoneId.systemDefault();
         long now = LocalDate.now().atStartOfDay().atZone(defaultZone).toInstant().toEpochMilli();
         long envNow = env.currentLocalDate().atStartOfDay().atZone(defaultZone).toInstant().toEpochMilli();
@@ -84,7 +86,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentLocalDate_ForAllAvailableZoneIds() {
+    void testCurrentLocalDate_ForAllAvailableZoneIds() {
         Set<String> zoneIds = ZoneId.getAvailableZoneIds();
         zoneIds.forEach(zone -> {
             ZoneId zoneId = ZoneId.of(zone);
@@ -95,7 +97,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentLocalTime_InDefaultTimeZone() {
+    void testCurrentLocalTime_InDefaultTimeZone() {
         ZoneId defaultZone = ZoneId.systemDefault();
         LocalDate currentLocalDate = LocalDate.now();
         long now = LocalTime.now().atDate(currentLocalDate).atZone(defaultZone).toInstant().toEpochMilli();
@@ -104,7 +106,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentLocalTime_ForAllAvailableZoneIds() {
+    void testCurrentLocalTime_ForAllAvailableZoneIds() {
         Set<String> zoneIds = ZoneId.getAvailableZoneIds();
         zoneIds.forEach(zone -> {
             ZoneId zoneId = ZoneId.of(zone);
@@ -116,7 +118,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentLocalDateTime_InDefaultTimeZone() {
+    void testCurrentLocalDateTime_InDefaultTimeZone() {
         ZoneId defaultZone = ZoneId.systemDefault();
         long now = LocalDateTime.now().atZone(defaultZone).toInstant().toEpochMilli();
         long envTime = env.currentLocalDateTime().atZone(defaultZone).toInstant().toEpochMilli();
@@ -124,7 +126,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentLocalDateTime_ForAllAvailableZoneIds() {
+    void testCurrentLocalDateTime_ForAllAvailableZoneIds() {
         Set<String> zoneIds = ZoneId.getAvailableZoneIds();
         zoneIds.forEach(zone -> {
             ZoneId zoneId = ZoneId.of(zone);
@@ -135,21 +137,21 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentZonedDateTimeUTC() {
+    void testCurrentZonedDateTimeUTC() {
         long now = ZonedDateTime.now(ZoneOffset.UTC).toInstant().toEpochMilli();
         long envTime = env.currentZonedDateTimeUTC().toInstant().toEpochMilli();
         assertNow(envTime, now);
     }
 
     @Test
-    public void testCurrentZonedDateTime_InDefaultTimeZone() {
+    void testCurrentZonedDateTime_InDefaultTimeZone() {
         long now = ZonedDateTime.now().toInstant().toEpochMilli();
         long envNow = env.currentZonedDateTime().toInstant().toEpochMilli();
         assertNow(envNow, now);
     }
 
     @Test
-    public void testCurrentZonedDateTime_ForAllAvailableZoneIds() {
+    void testCurrentZonedDateTime_ForAllAvailableZoneIds() {
         Set<String> zoneIds = ZoneId.getAvailableZoneIds();
         zoneIds.forEach(zone -> {
             ZoneId zoneId = ZoneId.of(zone);
@@ -160,14 +162,14 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentTimeMillis() {
+    void testCurrentTimeMillis() {
         long now = System.currentTimeMillis();
         long envNow = env.currentTimeMillis();
         assertNow(envNow, now);
     }
 
     @Test
-    public void testNanoTime() {
+    void testNanoTime() {
         long now = System.nanoTime();
         long envNano = env.nanoTime();
         long deltaNanos = TimeUnit.MICROSECONDS.toNanos(100);
@@ -188,7 +190,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testCurrentProcessId() {
+    void testCurrentProcessId() {
         String pid = env.currentProcessId();
         assertThat(pid).isNotNull();
         assertIsNumeric(pid);
@@ -200,7 +202,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testTryGetCurrentProcessId() {
+    void testTryGetCurrentProcessId() {
         Optional<Integer> optionalPid = env.tryGetCurrentProcessId();
         assertThat(optionalPid).isNotNull();
         assertThat(optionalPid)
@@ -209,7 +211,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testSleep() throws InterruptedException {
+    void testSleep() throws InterruptedException {
         long sleepTime = 50;
         long start = System.currentTimeMillis();
         env.sleep(sleepTime);
@@ -218,7 +220,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testSleep_UsingTimeUnit() throws InterruptedException {
+    void testSleep_UsingTimeUnit() throws InterruptedException {
         long sleepTime = 50;
         long start = System.currentTimeMillis();
         env.sleep(sleepTime, TimeUnit.MILLISECONDS);
@@ -227,7 +229,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testSleep_WithNanos() throws InterruptedException {
+    void testSleep_WithNanos() throws InterruptedException {
         long sleepMillis = 50;
         int sleepNanos = 999_999;
         long start = System.currentTimeMillis();
@@ -237,7 +239,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testSleepQuietly() {
+    void testSleepQuietly() {
         long sleepTime = 50;
         long start = System.currentTimeMillis();
         boolean interrupted = env.sleepQuietly(sleepTime);
@@ -247,7 +249,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testSleepQuietly_UsingTimeUnit() {
+    void testSleepQuietly_UsingTimeUnit() {
         long sleepTime = 50;
         long start = System.currentTimeMillis();
         boolean interrupted = env.sleepQuietly(sleepTime, TimeUnit.MILLISECONDS);
@@ -257,7 +259,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testSleepQuietly_WithNanos() {
+    void testSleepQuietly_WithNanos() {
         long sleepMillis = 50;
         int sleepNanos = 999_999;
         long start = System.currentTimeMillis();
@@ -278,7 +280,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testGetEnv_ForSingleNamedEnvironmentVariable() {
+    void testGetEnv_ForSingleNamedEnvironmentVariable() {
         Map<String, String> actualEnv = System.getenv();
         actualEnv.forEach((name, actualValue) -> {
             String value = env.getenv(name);
@@ -289,12 +291,12 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testGetEnv_ToRetrieveAllEnvironmentVariablesAsMap() {
+    void testGetEnv_ToRetrieveAllEnvironmentVariablesAsMap() {
         assertThat(env.getenv()).isEqualTo(System.getenv());
     }
 
     @Test
-    public void testGetProperty() {
+    void testGetProperty() {
         Properties props = System.getProperties();
         props.forEach((key, actualValue) -> {
             String value = env.getProperty(String.valueOf(key));
@@ -305,7 +307,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testGetProperty_WithDefaultValue() {
+    void testGetProperty_WithDefaultValue() {
         KiwiMaps.<String, String>newHashMap(
                 "foo", "the default of foo",
                 "bar", "the default of bar",
@@ -324,7 +326,7 @@ public class DefaultKiwiEnvironmentTest {
     }
 
     @Test
-    public void testGetProperties() {
+    void testGetProperties() {
         Properties props = env.getProperties();
         assertThat(props).isEqualTo(System.getProperties());
     }
