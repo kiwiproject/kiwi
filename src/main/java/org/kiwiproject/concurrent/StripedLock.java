681464d395c6d8c3a0c66c0cc695f353b577108e
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-03-20 21:14:41 -0400

    Add StripedLock (#60)
    Fixes #53

---
+++ src/main/java/org/kiwiproject/concurrent/StripedLock.java
@@ -0,0 +1,143 @@
+package org.kiwiproject.concurrent;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import com.google.common.util.concurrent.Striped;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.function.Supplier;
+
+/**
+ * {@link StripedLock} provides simple lambdas for encapsulating a block of code with a read/write lock.
+ * <p>
+ * The number of "stripes" indicates the maximum concurrent processes where the lock "key" is hashed and is used to
+ * select a lock. This is useful when you want tasks that operate in the same "context" to block one another without
+ * blocking unrelated tasks.
+ *
+ * @implNote This {@link StripedLock} uses Guava's {@link Striped} under the covers. The {@link ReadWriteLock}s
+ * are re-entrant, and read locks can be held by multiple readers, while write locks are exclusive.
+ * @see Striped
+ * @see ReadWriteLock
+ */
+@SuppressWarnings("UnstableApiUsage")  // Guava's Striped is marked @Beta (but has been in Guava since 13, a long time)
+@Slf4j
+public class StripedLock {
+
+    private static final String DEFAULT_KEY_WHEN_BLANK = "BLANK-LOCK-KEY";
+    private static final int DEFAULT_NUM_STRIPES = Runtime.getRuntime().availableProcessors() * 4;
+
+    private final Striped<ReadWriteLock> lock;
+
+    /**
+     * Creates a new {@link StripedLock}, using {@link #DEFAULT_KEY_WHEN_BLANK} as the number of stripes.
+     */
+    public StripedLock() {
+        this(Striped.readWriteLock(DEFAULT_NUM_STRIPES));
+    }
+
+    /**
+     * Creates a new {@link StripedLock} with the given number of stripes.
+     */
+    public StripedLock(int numStripes) {
+        this(Striped.readWriteLock(numStripes));
+    }
+
+    /**
+     * Create a new {@link StripedLock} using the given {@link ReadWriteLock}. This is useful if you have
+     * multiple interdependent usages that you want to share across the same set of locks.
+     */
+    public StripedLock(Striped<ReadWriteLock> lock) {
+        this.lock = lock;
+    }
+
+    /**
+     * Execute a {@link Runnable} task using the provided lock key and associated a READ lock.
+     * <p>
+     * <em>This implementation will block until the read lock is acquired.</em>
+     *
+     * @param lockKey the lock key
+     * @param task    the task to run
+     */
+    public void runWithReadLock(String lockKey, Runnable task) {
+        supplyWithReadLock(lockKey, () -> {
+            task.run();
+            return null;
+        });
+    }
+
+    /**
+     * Execute a {@link Supplier} using the provided lock key and associated READ lock.
+     * <p>
+     * <em>This implementation will block until the read lock is acquired.</em>
+     *
+     * @param lockKey the lock key
+     * @param task    the task to supply a value
+     * @param <T>     the type of object being supplied
+     * @return the supplied value
+     */
+    public <T> T supplyWithReadLock(String lockKey, Supplier<T> task) {
+        var nonNullKey = ensureNonBlankKey(lockKey);
+        var readWriteLock = lock.get(nonNullKey);
+        var lockHashCode = extractHashCode(readWriteLock);
+        LOG.trace("Locking read lock {} for key {}", lockHashCode, nonNullKey);
+        readWriteLock.readLock().lock();
+
+        try {
+            LOG.trace("Running task with read lock {} for key {}", lockHashCode, nonNullKey);
+            return task.get();
+        } finally {
+            LOG.trace("Unlocking read lock {} for key {}", lockHashCode, nonNullKey);
+            readWriteLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Execute a {@link Runnable} task using the provided lock key and associated a WRITE lock.
+     * <p>
+     * <em>This implementation will block until the write lock is acquired.</em>
+     *
+     * @param lockKey the lock key
+     * @param task    the task to run
+     */
+    public void runWithWriteLock(String lockKey, Runnable task) {
+        supplyWithWriteLock(lockKey, () -> {
+            task.run();
+            return null;
+        });
+    }
+
+    /**
+     * Execute a {@link Supplier} using the provided lock key and associated WRITE lock.
+     * <p>
+     * <em>This implementation will block until the write lock is acquired.</em>
+     *
+     * @param lockKey the lock key
+     * @param task    the task to supply a value
+     * @param <T>     the type of object being supplied
+     * @return the supplied value
+     */
+    public <T> T supplyWithWriteLock(String lockKey, Supplier<T> task) {
+        var nonNullKey = ensureNonBlankKey(lockKey);
+        var readWriteLock = lock.get(nonNullKey);
+        var lockHashCode = extractHashCode(readWriteLock);
+        LOG.trace("Locking write lock {} for key {}", lockHashCode, nonNullKey);
+        readWriteLock.writeLock().lock();
+
+        try {
+            LOG.trace("Running task with write lock {} for key {}", lockHashCode, nonNullKey);
+            return task.get();
+        } finally {
+            LOG.trace("Unlocking write lock {} for key {}", lockHashCode, nonNullKey);
+            readWriteLock.writeLock().unlock();
+        }
+    }
+
+    private String ensureNonBlankKey(String key) {
+        return isBlank(key) ? DEFAULT_KEY_WHEN_BLANK : key;
+    }
+
+    private String extractHashCode(Object obj) {
+        return "@" + Integer.toHexString(System.identityHashCode(obj));
+    }
+}
---
+++ src/test/java/org/kiwiproject/concurrent/StripedLockTest.java
@@ -0,0 +1,261 @@
+package org.kiwiproject.concurrent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.kiwiproject.collect.KiwiLists.first;
+import static org.kiwiproject.collect.KiwiLists.second;
+
+import com.google.common.collect.Range;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
+
+@DisplayName("StripedLock")
+@Slf4j
+class StripedLockTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void testRunWithReadLock_OnNullAndEmptyKeys(String value) {
+        var flag = new AtomicBoolean();
+        new StripedLock().runWithReadLock(value, () -> flag.set(true));
+
+        assertThat(flag).isTrue();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void testRunWithWriteLock_OnNullAndEmptyKeys(String value) {
+        var flag = new AtomicBoolean();
+        new StripedLock().runWithWriteLock(value, () -> flag.set(true));
+
+        assertThat(flag).isTrue();
+    }
+
+    @Test
+    void testRunWithReadLock_InSameThread_WithMultipleReadLocks() {
+        var stripedLock = new StripedLock(1);
+        var flag = new AtomicBoolean();
+
+        stripedLock.runWithReadLock("test", () ->
+                stripedLock.runWithReadLock("test", () ->
+                        flag.set(true)));
+
+        assertThat(flag).isTrue();
+    }
+
+    @Test
+    void testRunWithWriteLock_InSameThread_WithMultipleWriteLocks() {
+        var stripedLock = new StripedLock(1);
+        var flag = new AtomicBoolean();
+
+        stripedLock.runWithWriteLock("test", () ->
+                stripedLock.runWithWriteLock("test", () ->
+                        flag.set(true)));
+
+        assertThat(flag).isTrue();
+    }
+
+    @Test
+    void testRunWithLock_WhenBlocking_ForWriteAndWriteLocks() {
+        var lock = new StripedLock(1);
+        var task = createRunnableTask();
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.runWithWriteLock(recorder1.id, () -> recorder1.record(task))),
+                Async.doAsync(() -> lock.runWithReadLock(recorder2.id, () -> recorder2.record(task)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, false);
+    }
+
+    @Test
+    void testRunWithLock_WhenNonBlocking_ForWriteAndWriteLocks() {
+        var lock = new StripedLock(2);
+        var task = createRunnableTask();
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.runWithWriteLock(recorder1.id, () -> recorder1.record(task))),
+                Async.doAsync(() -> lock.runWithReadLock(recorder2.id, () -> recorder2.record(task)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, true);
+    }
+
+    @Test
+    void testRunWithLock_WhenBlocking_ForReadAndWriteLocks() {
+        var lock = new StripedLock(1);  // ensures blocking execution
+        var task = createRunnableTask();
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.runWithWriteLock(recorder1.id, () -> recorder1.record(task))),
+                Async.doAsync(() -> lock.runWithReadLock(recorder2.id, () -> recorder2.record(task)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, false);
+    }
+
+    @Test
+    void testRunWithLock_WhenNonBlocking_ForReadAndReadLocks() {
+        var lock = new StripedLock(1);
+        var task = createRunnableTask();
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.runWithReadLock(recorder1.id, () -> recorder1.record(task))),
+                Async.doAsync(() -> lock.runWithReadLock(recorder2.id, () -> recorder2.record(task)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, true);
+    }
+
+    @Test
+    void testSupplyWithLock_WhenNonBlocking_ForReadAndReadLocks() {
+        var lock = new StripedLock(1);
+        var task1 = createSupplierTask(42L);
+        var task2 = createSupplierTask(84L);
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.supplyWithReadLock(recorder1.id, () -> recorder1.recordAndSupply(task1))),
+                Async.doAsync(() -> lock.supplyWithReadLock(recorder2.id, () -> recorder2.recordAndSupply(task2)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, true);
+
+        assertThat(first(completableFutures).getNow(-1L)).isEqualTo(42L);
+        assertThat(second(completableFutures).getNow(-1L)).isEqualTo(84L);
+    }
+
+    @Test
+    void testSupplyWithLock_WhenBlocking_ForWriteAndReadLocks() {
+        var lock = new StripedLock(1);
+        var task1 = createSupplierTask(42L);
+        var task2 = createSupplierTask(84L);
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.supplyWithWriteLock(recorder1.id, () -> recorder1.recordAndSupply(task1))),
+                Async.doAsync(() -> lock.supplyWithReadLock(recorder2.id, () -> recorder2.recordAndSupply(task2)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, false);
+
+        assertThat(first(completableFutures).getNow(-1L)).isEqualTo(42L);
+        assertThat(second(completableFutures).getNow(-1L)).isEqualTo(84L);
+    }
+
+    @Test
+    void testSupplyWithLock_WhenNonBlocking_ForReadAndWriteLocks() {
+        var lock = new StripedLock(2);
+        var task1 = createSupplierTask(42L);
+        var task2 = createSupplierTask(84L);
+        var recorder1 = new TaskRecorder("task1");
+        var recorder2 = new TaskRecorder("task2");
+
+        var completableFutures = List.of(
+                Async.doAsync(() -> lock.supplyWithReadLock(recorder1.id, () -> recorder1.recordAndSupply(task1))),
+                Async.doAsync(() -> lock.supplyWithWriteLock(recorder2.id, () -> recorder2.recordAndSupply(task2)))
+        );
+
+        waitForAll(completableFutures);
+
+        logAndCheckExecutionTimes(recorder1, recorder2, true);
+
+        assertThat(first(completableFutures).getNow(-1L)).isEqualTo(42L);
+        assertThat(second(completableFutures).getNow(-1L)).isEqualTo(84L);
+    }
+
+    private Runnable createRunnableTask() {
+        return this::sleep100ms;
+    }
+
+    private Supplier<Long> createSupplierTask(Long result) {
+        return () -> {
+            sleep100ms();
+            return result;
+        };
+    }
+
+    private void sleep100ms() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            LOG.error("Unexpected InterruptedException", e);
+            fail("Failed due to InterruptedException");
+        }
+    }
+
+    private <T> void waitForAll(List<CompletableFuture<T>> futures) {
+        Async.waitForAll(futures, 300, TimeUnit.MILLISECONDS);
+    }
+
+    private void logAndCheckExecutionTimes(TaskRecorder recorder1, TaskRecorder recorder2, boolean expectedOverlap) {
+        LOG.debug("task 1 execution time range: {}", recorder1.timeRange());
+        LOG.debug("task 2 execution time range: {}", recorder2.timeRange());
+
+        assertThat(recorder1.overlaps(recorder2)).isEqualTo(expectedOverlap);
+    }
+
+    static class TaskRecorder {
+
+        private final String id;
+        private Instant start;
+        private Instant stop;
+
+        TaskRecorder(String id) {
+            this.id = id;
+        }
+
+        void record(Runnable task) {
+            start = Instant.now();
+            task.run();
+            stop = Instant.now();
+        }
+
+        <T> T recordAndSupply(Supplier<T> supplier) {
+            start = Instant.now();
+            var result = supplier.get();
+            stop = Instant.now();
+            return result;
+        }
+
+        Range<Long> timeRange() {
+            return Range.open(start.toEpochMilli(), stop.toEpochMilli());
+        }
+
+        boolean overlaps(TaskRecorder other) {
+            return timeRange().isConnected(other.timeRange());
+        }
+    }
+}
