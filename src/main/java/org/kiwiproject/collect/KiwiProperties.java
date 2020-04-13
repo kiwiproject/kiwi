a9b075e1957f222cb9e8cb2c3ab6f636b4271609
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-04-13 13:06:04 -0400

    Updated the KiwiProperties utility and tests
    Fixes #73

--- src/main/java/org/kiwiproject/collect/KiwiProperties.java
+++ src/main/java/org/kiwiproject/collect/KiwiProperties.java
@@ -1,42 +1,91 @@
 package org.kiwiproject.collect;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.partition;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
+import static org.kiwiproject.base.KiwiPreconditions.checkEvenItemCount;
+import static org.kiwiproject.collect.KiwiLists.first;
+import static org.kiwiproject.collect.KiwiLists.second;
+
 import lombok.experimental.UtilityClass;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.util.Objects.requireNonNull;
-import static org.kiwiproject.base.KiwiPreconditions.checkEvenItemCount;
-import static org.kiwiproject.collect.KiwiLists.first;
-import static org.kiwiproject.collect.KiwiLists.second;
-
+/**
+ * Utility methods for making it easier to create and work with {@link java.util.Properties} instances.
+ */
 @UtilityClass
 public class KiwiProperties {
 
+    /**
+     * Crates a <i>mutable</i> {@link Properties} instance by parsing the items argument in pairs.
+     * The items argument contains keys and values in the form:
+     * <p>
+     * <i>key-1, value-1, key-2, value-2, ... , key-N, value-N</i>
+     *
+     * @param items the items containing keys and values, in pairs
+     * @return a new Properties instance with data from items
+     * @throws NullPointerException if any of the items is null
+     * @implNote Please note that even though this method accepts objects, you should really only put strings in here,
+     * due to the way {@link Properties} is designed to only have string keys and values. The values are always converted
+     * to strings before putting them in the properties.
+     */
+    public static Properties newProperties(Object... items) {
+        checkEvenItemCount(items);
+        var strings = newArrayList(items).stream().map(Object::toString).collect(toList());
+        var kvPairs = partition(strings, 2);
+        return newPropertiesFromStringPairs(kvPairs);
+    }
+
+    /**
+     * Creates a <i>mutable</i> {@link Properties} instance by parsing the items argument in pairs.
+     * The items argument contains keys and values in the form:
+     * <p>
+     * <i>key-1, value-1, key-2, value-2, ... , key-N, value-N</i>
+     * </p>
+     *
+     * @param items the items containing keys and values, in pairs
+     * @return a new Properties instance with data from items
+     * @throws NullPointerException if any of the items is null
+     */
     public static Properties newProperties(String... items) {
         checkEvenItemCount(items);
-        Properties properties = new Properties();
-        for (int i = 0; i < items.length; i += 2) {
-            String name = items[i];
-            String value = items[i + 1];
-            properties.setProperty(name, value);
-        }
-        return properties;
+        var kvPairs = partition(newArrayList(items), 2);
+        return newPropertiesFromStringPairs(kvPairs);
     }
 
+    /**
+     * Creates a <i>mutable</i> {@link Properties} instance by parsing the items argument in pairs from the list.
+     *
+     * @param items the items containing keys and values, in pairs
+     * @return a new Properties instance with data from items
+     * @throws NullPointerException if any of the items is null
+     */
     public static Properties newProperties(List<String> items) {
         checkEvenItemCount(items);
-        Properties properties = new Properties();
-        for (int i = 0; i < items.size(); i += 2) {
-            String name = items.get(i);
-            String value = items.get(i + 1);
-            properties.setProperty(name, value);
-        }
-        return properties;
+        var kvPairs = partition(items, 2);
+        return newPropertiesFromStringPairs(kvPairs);
     }
 
+    /**
+     * Makes creating a {@link Properties} instance from a {@link Map} a one-liner. Also, restricts the map to string
+     * keys and values. Creates a <i>mutable</i> {@link Properties} instance.
+     *
+     * @param map the source map
+     * @return a new Properties instance with data from the map
+     * @throws NullPointerException if any of the items is null
+     * @implNote The reason this method restricts the map keys and values to strings is because Properties is derived
+     * from {@link java.util.Hashtable} and the JavaDoc states:
+     * "Because {@code Properties} inherits from {@code Hashtable}, the {@code put} and {@code putAll} methods can be
+     * applied to a {@code Properties} object. Their use is strongly discouraged as they allow the caller to insert
+     * entries whose keys or values are not {@code Strings}. The {@code setProperty} method should be used instead."
+     * This of course is simply poor design (they should have used composition and hidden the internal storage details
+     * instead of extending Hashtable).
+     */
     public static Properties newProperties(Map<String, String> map) {
         requireNonNull(map);
         Properties properties = new Properties();
@@ -44,6 +93,21 @@ public class KiwiProperties {
         return properties;
     }
 
+    /**
+     * Creates a <i>mutable</i> {@link Properties} instance from each key/value pair list inside the outer list.
+     * The items argument must contain keys and values in teh form:
+     * <p>
+     * <i>[ [key-1, value-1], [key-2, value-2], ... , [key-N, value-N]</i>
+     * </p>
+     *
+     * @param items the items list containing a series of two-items key/value pair lists
+     * @return a new Properties instance with data from items
+     * @throws NullPointerException if any of the items is null
+     * @throws IllegalArgumentException if any of the key/value pair lists do not have at least two elements
+     * @implNote only the first and second elements of the key/value pair sub-lists are used when creating the
+     * Properties instance. Thus while it does not make much sense, this method will not throw an exception if the
+     * sub-lists contain more than two elements.
+     */
     public static Properties newPropertiesFromStringPairs(List<List<String>> items) {
         requireNonNull(items);
         return items.stream().collect(
@@ -53,7 +117,7 @@ public class KiwiProperties {
     }
 
     private static void checkPairAndAccumulate(Properties accumulator, List<String> pair) {
-        checkArgument(pair.size() == 2, "Each sublist must contain exactly 2 items");
+        checkArgument(pair.size() >= 2, "Each sublist must contain at least 2 items");
         accumulator.setProperty(first(pair), second(pair));
     }
 
--- src/test/java/org/kiwiproject/collect/KiwiPropertiesTest.java
+++ src/test/java/org/kiwiproject/collect/KiwiPropertiesTest.java
@@ -1,32 +1,32 @@
 package org.kiwiproject.collect;
 
-import org.junit.Test;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.entry;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.entry;
-
-public class KiwiPropertiesTest {
+class KiwiPropertiesTest {
 
     @Test
-    public void testNewProperties_UsingVarArgs_WhenNoItems() {
+    void testNewProperties_UsingVarArgs_WhenNoItems() {
         assertThat(KiwiProperties.newProperties()).isEmpty();
     }
 
     @Test
-    public void testNewProperties_UsingVarArgs_WhenOddNumberOfItems() {
+    void testNewProperties_UsingVarArgs_WhenOddNumberOfItems() {
         assertThatThrownBy(() -> KiwiProperties.newProperties("a", "A", "b"))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void testNewProperties_UsingVarArgs_WhenEvenNumberOfItems() {
+    void testNewProperties_UsingVarArgs_WhenEvenNumberOfItems() {
         Properties properties = KiwiProperties.newProperties("a", "A", "b", "B", "c", "C", "d", "D");
         assertThat(properties).containsOnly(
                 entry("a", "A"),
@@ -37,18 +37,18 @@ public class KiwiPropertiesTest {
     }
 
     @Test
-    public void testNewProperties_UsingList_WhenNoItems() {
+    void testNewProperties_UsingList_WhenNoItems() {
         assertThat(KiwiProperties.newProperties(new ArrayList<>())).isEmpty();
     }
 
     @Test
-    public void testNewProperties_UsingList_WhenOddNumberOfItems() {
+    void testNewProperties_UsingList_WhenOddNumberOfItems() {
         assertThatThrownBy(() -> KiwiProperties.newProperties(newArrayList("a", "A", "b")))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void testNewProperties_UsingList_WhenEvenNumberOfItems() {
+    void testNewProperties_UsingList_WhenEvenNumberOfItems() {
         Properties properties = KiwiProperties.newProperties(newArrayList("a", "A", "b", "B", "c", "C", "d", "D"));
         assertThat(properties).containsOnly(
                 entry("a", "A"),
@@ -59,12 +59,12 @@ public class KiwiPropertiesTest {
     }
 
     @Test
-    public void testNewProperties_UsingMap_WhenNoItems() {
+    void testNewProperties_UsingMap_WhenNoItems() {
         assertThat(KiwiProperties.newProperties(new HashMap<>())).isEmpty();
     }
 
     @Test
-    public void testNewProperties_UsingMap() {
+    void testNewProperties_UsingMap() {
         Properties properties = KiwiProperties.newProperties(KiwiMaps.newHashMap("a", "A", "b", "B", "c", "C", "d", "D"));
         assertThat(properties).containsOnly(
                 entry("a", "A"),
@@ -75,12 +75,12 @@ public class KiwiPropertiesTest {
     }
 
     @Test
-    public void testNewPropertiesFromStringPairs_WhenNoItems() {
+    void testNewPropertiesFromStringPairs_WhenNoItems() {
         assertThat(KiwiProperties.newPropertiesFromStringPairs(newArrayList())).isEmpty();
     }
 
     @Test
-    public void testNewPropertiesFromStringPairs_WhenBadArgument() {
+    void testNewPropertiesFromStringPairs_WhenBadArgument() {
         List<List<String>> items = newArrayList(
                 newArrayList("a", "1"),
                 newArrayList("b", "2"),
@@ -89,11 +89,11 @@ public class KiwiPropertiesTest {
         );
         assertThatThrownBy(() -> KiwiProperties.newPropertiesFromStringPairs(items))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Each sublist must contain exactly 2 items");
+                .hasMessage("Each sublist must contain at least 2 items");
     }
 
     @Test
-    public void testNewPropertiesFromStringPairs() {
+    void testNewPropertiesFromStringPairs() {
         List<List<String>> items = newArrayList(
                 newArrayList("a", "1"),
                 newArrayList("b", "2"),
@@ -107,6 +107,44 @@ public class KiwiPropertiesTest {
                 entry("c", "3"),
                 entry("d", "4")
         );
+    }
+
+    @Test
+    void testNewPropertiesFromStringPairs_WhenExtraValuesInPairs() {
+        List<List<String>> items = newArrayList(
+                newArrayList("a", "1", "extra"),
+                newArrayList("b", "2"),
+                newArrayList("c", "3", "anotherExtra", "another Value"),
+                newArrayList("d", "4")
+        );
+        Properties properties = KiwiProperties.newPropertiesFromStringPairs(items);
+        assertThat(properties).containsOnly(
+                entry("a", "1"),
+                entry("b", "2"),
+                entry("c", "3"),
+                entry("d", "4")
+        );
+    }
+
+    @Test
+    void testNewProperties_WithEmptyObjectArray() {
+        var objArr = new Object[0];
+        assertThat(KiwiProperties.newProperties(objArr)).isEmpty();
+    }
+
+    @Test
+    void testNewProperties_UsingObjectArray_WhenOddNumberOfItems() {
+        assertThatThrownBy(() -> KiwiProperties.newProperties("a", 42, "b"))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testNewProperties_WithObjectArray_WhenEvenNumberOfItems() {
+        var props = KiwiProperties.newProperties("k1", 42, "k2", "a string", "k3", true);
+        assertThat(props).hasSize(3);
+        assertThat(props.getProperty("k1")).isEqualTo("42");
+        assertThat(props.getProperty("k2")).isEqualTo("a string");
+        assertThat(props.getProperty("k3")).isEqualTo("true");
     }
 
 }
