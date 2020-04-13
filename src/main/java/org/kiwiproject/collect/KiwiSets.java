05610f93f081ba634d839b5d1101807e90e96179
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-04-13 15:04:12 -0400

    Added javadocs to the existing methods in KiwiSets (#79)
    Also converted tests to JUnit 5
    
    Fixes #77

--- src/main/java/org/kiwiproject/collect/KiwiSets.java
+++ src/main/java/org/kiwiproject/collect/KiwiSets.java
@@ -1,24 +1,48 @@
 package org.kiwiproject.collect;
 
+import static java.util.Objects.nonNull;
+
 import lombok.experimental.UtilityClass;
 
 import java.util.Set;
 
-import static java.util.Objects.nonNull;
-
+/**
+ * Utility methods for working with {@link Set} instances.
+ */
 @UtilityClass
 public class KiwiSets {
 
-    public static <T> boolean isNullOrEmpty(Set<T> items) {
-        return items == null || items.isEmpty();
+    /**
+     * Checks whether the specified set is null or empty.
+     *
+     * @param set the set
+     * @param <T> the type of items in the set
+     * @return {@code true} if set is null or empty; {@code false} otherwise
+     */
+    public static <T> boolean isNullOrEmpty(Set<T> set) {
+        return set == null || set.isEmpty();
     }
 
-    public static <T> boolean isNotNullOrEmpty(Set<T> items) {
-        return !isNullOrEmpty(items);
+    /**
+     * Checks whether the specified is neither null nor empty.
+     *
+     * @param set the set
+     * @param <T> the type of items in the set
+     * @return {@code true} if set is neither null nor empty; {@code false} otherwise
+     */
+    public static <T> boolean isNotNullOrEmpty(Set<T> set) {
+        return !isNullOrEmpty(set);
     }
 
-    public static <T> boolean hasOneElement(Set<T> items) {
-        return nonNull(items) && items.size() == 1;
+    /**
+     * Checks whether the specified list is non-null and has only one item.
+     *
+     * @param set the set
+     * @param <T> the type of items in the set
+     * @return {@code true} if list is non-null and has exactly one item; {@code false}
+     */
+    public static <T> boolean hasOneElement(Set<T> set) {
+        return nonNull(set) && set.size() == 1;
     }
 
 }
--- src/test/java/org/kiwiproject/collect/KiwiSetsTest.java
+++ src/test/java/org/kiwiproject/collect/KiwiSetsTest.java
@@ -1,64 +1,66 @@
 package org.kiwiproject.collect;
 
-import org.junit.Test;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static com.google.common.collect.Sets.newHashSet;
-import static org.assertj.core.api.Assertions.assertThat;
+class KiwiSetsTest {
 
-public class KiwiSetsTest {
-
+    @SuppressWarnings("ConstantConditions")
     @Test
-    public void testIsNullOrEmpty_WhenNull() {
+    void testIsNullOrEmpty_WhenNull() {
         assertThat(KiwiSets.isNullOrEmpty(null)).isTrue();
     }
 
     @Test
-    public void testIsNullOrEmpty_WhenEmpty() {
+    void testIsNullOrEmpty_WhenEmpty() {
         assertThat(KiwiSets.isNullOrEmpty(new HashSet<>())).isTrue();
     }
 
     @Test
-    public void testIsNullOrEmpty_WhenContainsSomeElements() {
+    void testIsNullOrEmpty_WhenContainsSomeElements() {
         Set<String> fruits = newSetOfFruits();
         assertThat(KiwiSets.isNullOrEmpty(fruits)).isFalse();
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Test
-    public void testIsNotNullOrEmpty_WhenNull() {
+    void testIsNotNullOrEmpty_WhenNull() {
         assertThat(KiwiSets.isNotNullOrEmpty(null)).isFalse();
     }
 
     @Test
-    public void testIsNotNullOrEmpty_WhenEmpty() {
+    void testIsNotNullOrEmpty_WhenEmpty() {
         assertThat(KiwiSets.isNotNullOrEmpty(new HashSet<>())).isFalse();
     }
 
     @Test
-    public void testIsNotNullOrEmpty_WhenContainsElements() {
+    void testIsNotNullOrEmpty_WhenContainsElements() {
         Set<String> fruits = newSetOfFruits();
         assertThat(KiwiSets.isNotNullOrEmpty(fruits)).isTrue();
     }
 
     @Test
-    public void testHasOneElement_WhenNull() {
+    void testHasOneElement_WhenNull() {
         assertThat(KiwiSets.hasOneElement(null)).isFalse();
     }
 
     @Test
-    public void testHasOneElement_WhenEmpty() {
+    void testHasOneElement_WhenEmpty() {
         assertThat(KiwiSets.hasOneElement(new HashSet<>())).isFalse();
     }
 
     @Test
-    public void testHasOneElement_WhenHasOnlyOneElement() {
+    void testHasOneElement_WhenHasOnlyOneElement() {
         assertThat(KiwiSets.hasOneElement(newHashSet("kiwi"))).isTrue();
     }
 
     @Test
-    public void testHasOneElement_WhenHasMoreThanOneElement() {
+    void testHasOneElement_WhenHasMoreThanOneElement() {
         assertThat(KiwiSets.hasOneElement(newSetOfFruits())).isFalse();
     }
 
