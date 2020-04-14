e4d05442276f26cabadbac78a888836fa3fa57d5
Scott Leberknight <scottleber@gmail.com>
2020-04-14 18:20:51 -0400

    I am dumb. Forgot to actually commit things that are required.

---
+++ src/test/java/org/kiwiproject/validation/ValidationTestHelper.java
@@ -0,0 +1,69 @@
+package org.kiwiproject.validation;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import lombok.experimental.UtilityClass;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Arrays;
+
+@UtilityClass
+public class ValidationTestHelper {
+
+    public static final Validator DEFAULT_VALIDATOR = newValidator();
+
+    public static Validator newValidator() {
+        return Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    public static void assertNoViolations(Validator validator, Object object) {
+        var violations = validator.validate(object);
+        assertThat(violations).isEmpty();
+    }
+
+    public static void assertViolations(Validator validator, Object object, int numExpectedViolations) {
+        var violations = validator.validate(object);
+        assertThat(violations).hasSize(numExpectedViolations);
+    }
+
+    public static void assertOnePropertyViolation(Validator validator, Object object, String propertyName) {
+        assertPropertyViolations(validator, object, propertyName, 1);
+    }
+
+    public static void assertNoPropertyViolations(Validator validator, Object object, String propertyName) {
+        assertPropertyViolations(validator, object, propertyName, 0);
+    }
+
+    public static void assertPropertyViolations(Validator validator,
+                                                Object object,
+                                                String propertyName,
+                                                int numExpectedViolations) {
+        var violations = validator.validateProperty(object, propertyName);
+        assertThat(violations).hasSize(numExpectedViolations);
+    }
+
+    public static void assertPropertyViolations(Validator validator,
+                                                Object object,
+                                                String propertyName,
+                                                String... expectedMessages) {
+
+        var violations = validator.validateProperty(object, propertyName);
+
+        var actualMessages = violations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(toUnmodifiableList());
+
+        var missingMessages = Arrays.stream(expectedMessages)
+                .filter(value -> !actualMessages.contains(value))
+                .collect(toUnmodifiableList());
+
+        if (!missingMessages.isEmpty()) {
+            fail("Messages [%s] not found in actual messages %s",
+                    String.join(",", missingMessages), actualMessages);
+        }
+    }
+}
