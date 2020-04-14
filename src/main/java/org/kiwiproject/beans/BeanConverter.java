0e1b0233316d408fa5176ae4952b6980817c5854
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-04-14 13:04:14 -0400

    Added the BeanConverter class
    Fixes #80

---
+++ src/main/java/org/kiwiproject/beans/BeanConverter.java
@@ -0,0 +1,173 @@
+package org.kiwiproject.beans;
+
+import static java.util.Objects.isNull;
+import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toSet;
+
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.NotReadablePropertyException;
+import org.springframework.beans.NotWritablePropertyException;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+@Slf4j
+public class BeanConverter<T> {
+
+    @Getter
+    private Map<String, Function<T, ?>> mappers = new HashMap<>();
+
+    @Getter
+    @Setter
+    private Set<String> exclusionList = Sets.newHashSet("class", "new");
+
+    @Getter
+    @Setter
+    private boolean failOnError;
+
+    /**
+     * This conversion method taks a single parameter and modifies the object in place.
+     *
+     * @param input the object to perform the conversion on
+     * @return the same object that was passed in
+     */
+    public T convert(T input) {
+        return convert(input, input);
+    }
+
+    /**
+     * This conversion method takes two parameters and copies properties from one object to another
+     *
+     * @param input   the object to copy the properties from
+     * @param target  the object to copy the properties too (destination)
+     * @param <R>     the type of object being returned
+     * @return the modified target object
+     */
+    @SuppressWarnings("unchecked")
+    public <R> R convert(T input, R target) {
+        if (isNull(input)) {
+            return null;
+        }
+
+        // perform standard mapping first
+        var inputWrapper = new BeanWrapperImpl(input);
+        var targetWrapper = new BeanWrapperImpl(target);
+
+        var propertyNames = getPropertyList(input, inputWrapper);
+
+        // This can not be a foreach because if failOnError is true, the exceptions need to bubble.
+        for (String propName : propertyNames) {
+            if (hasPropertyMapper(propName)) {
+                // custom mapper
+                var func = getPropertyMapper(propName);
+                func.apply(input);
+            } else if (input != target) {
+                // only need to map simple properties if it's not the same object
+                var inputValue = readBeanValue(input, inputWrapper, propName);
+                writeBeanValue(target, targetWrapper, propName, inputValue);
+
+            }
+        }
+
+        return target;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected Set<String> getPropertyList(T input, BeanWrapper inputWrapper) {
+        var propertyNames = Stream.of(inputWrapper.getPropertyDescriptors())
+                .map(PropertyDescriptor::getName)
+                .collect(toSet());
+
+        if (input instanceof Map) {
+            propertyNames.addAll(((Map<String, ?>) input).keySet());
+        }
+
+        // remove exclusions
+        return propertyNames.stream()
+                .filter(not(prop -> exclusionList.contains(prop)))
+                .collect(toSet());
+    }
+
+    /**
+     * Checks to see if a property mapper exists for a given property name
+     *
+     * @param propertyName the property name
+     * @return true if a property mapper was found for that property
+     */
+    public boolean hasPropertyMapper(String propertyName) {
+        return mappers.containsKey(propertyName);
+    }
+
+    /**
+     * Get the property mapper function for a specific property name
+     *
+     * @param propertyName the property name
+     * @return the Function that will be triggered
+     */
+    @SuppressWarnings("rawtypes")
+    public Function getPropertyMapper(String propertyName) {
+        return mappers.get(propertyName);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected Object readBeanValue(T input, BeanWrapper inputWrapper, String propName) {
+        try {
+            return inputWrapper.getPropertyValue(propName);
+        } catch (NotReadablePropertyException e) {
+            // try direct access if map
+            if (input instanceof Map) {
+                return ((Map<String, ?>) input).get(propName);
+            } else {
+                // if no approach works
+                logOrFail("Exception trying to read value", propName, e);
+            }
+        }
+
+        return null;
+    }
+
+    protected void logOrFail(String msg, String propName, RuntimeException e) {
+        if (failOnError) {
+            throw e;
+        } else {
+            LOG.debug("{} - property: {}", msg, propName);
+            LOG.trace("Exception: ", e);
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    protected <R> void writeBeanValue(R target, BeanWrapper targetWrapper, String propName, Object inputValue) {
+        try {
+            // default - grab value directly from input
+            targetWrapper.setPropertyValue(propName, inputValue);
+        } catch (NotWritablePropertyException e) {
+            // try direct access if map
+            if (target instanceof Map) {
+                ((Map) target).put(propName, inputValue);
+            } else {
+                // if no approach works
+                logOrFail("Exception trying to write value", propName, e);
+            }
+        }
+    }
+
+    /**
+     * Adds a property mapper function for a specific property name
+     *
+     * @param propertyName the property name
+     * @param function the Function that will be triggered
+     * @return true if successfully added; false otherwise
+     */
+    public boolean addPropertyMapper(String propertyName, Function<T, ?> function) {
+        return mappers.put(propertyName, function) != null;
+    }
+}
---
+++ src/test/java/org/kiwiproject/beans/BeanConverterTest.java
@@ -0,0 +1,150 @@
+package org.kiwiproject.beans;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.data.MapEntry.entry;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.kiwiproject.collect.KiwiMaps;
+import org.springframework.beans.TypeMismatchException;
+
+import java.util.Map;
+import java.util.Set;
+
+@DisplayName("BeanConverter")
+class BeanConverterTest {
+
+    private TestData constructTestData() {
+        return new TestData(1, "foo", KiwiMaps.newHashMap("innerFoo", "innerBar"));
+    }
+
+    @Test
+    void testBasicConvertMapToTargetType() {
+        var input = Map.of(
+                "numberField", 1,
+                "stringField", "foo",
+                "mapField", Map.of("innerFoo", "innerBar")
+        );
+
+        var converter = new BeanConverter<Map<String, Object>>();
+        var testData = converter.convert(input, new TestData());
+
+        assertThat(testData.getNumberField()).isEqualTo(1);
+        assertThat(testData.getStringField()).isEqualTo("foo");
+        assertThat(testData.getMapField()).contains(entry("innerFoo", "innerBar"));
+    }
+
+    @Test
+    void testBasicConvertTypeToMap() {
+        var converter = new BeanConverter<TestData>();
+
+        var output = converter.convert(constructTestData(), KiwiMaps.newHashMap());
+
+        assertThat(output).contains(
+                entry("numberField", 1),
+                entry("stringField", "foo"),
+                entry("mapField", Map.of("innerFoo", "innerBar"))
+        );
+    }
+
+    @Test
+    void testBasicConvertTypeToMap_WithExclusions() {
+        var converter = new BeanConverter<TestData>();
+        converter.setExclusionList(Set.of("numberField", "mapField"));
+
+        assertThat(converter.getExclusionList()).contains("numberField", "mapField");
+
+        var output = converter.convert(constructTestData(), KiwiMaps.newHashMap());
+
+        assertThat(output).contains(entry("stringField", "foo"));
+        assertThat(output).doesNotContainKeys("numberField", "mapField");
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    void testFailOnErrorHandling() {
+        var input = Map.of("numberField", "NOT A NUMBER");
+
+        var converter = new BeanConverter<Map>();
+        assertThat(converter.isFailOnError()).isFalse(); // default value
+
+        converter.setFailOnError(true);
+        assertThat(converter.isFailOnError()).isTrue();
+
+        assertThat(catchThrowable(() -> converter.convert(input, new TestData())))
+                .isExactlyInstanceOf(TypeMismatchException.class);
+    }
+
+    @Test
+    void testBasicConvertTypeToOtherType() {
+        var converter = new BeanConverter<TestData>();
+
+        var otherTestData = converter.convert(constructTestData(), new OtherTestData());
+
+        assertThat(otherTestData.getNumberField()).isEqualTo(1);
+        assertThat(otherTestData.getStringField()).isEqualTo("foo");
+        assertThat(otherTestData.getMapField()).contains(entry("innerFoo", "innerBar"));
+    }
+
+    @Test
+    void testConversionWithCustomRule() {
+        var converter = new BeanConverter<TestData>();
+        converter.addPropertyMapper("numberField", data -> {
+            // increment by 1000
+            var val = data.getNumberField() + 1000;
+            data.setNumberField(val);
+            return val;
+        });
+
+        converter.addPropertyMapper("stringField", data -> {
+            // prefix with 'test'
+            var val = "test:" + data.getStringField();
+            data.setStringField(val);
+            return val;
+        });
+
+        converter.addPropertyMapper("mapField", data -> {
+            var m = data.getMapField();
+            m.put("test", "1234");
+            return m;
+        });
+
+        var result = converter.convert(constructTestData());
+
+        assertThat(result.getNumberField()).isEqualTo(1001);
+        assertThat(result.getStringField()).isEqualTo("test:foo");
+        assertThat(result.getMapField()).contains(entry("innerFoo", "innerBar"), entry("test", "1234"));
+    }
+
+    @Test
+    void testConversionWithNullInput() {
+        var converter = new BeanConverter<TestData>();
+
+        assertThat(converter.convert(null)).isNull();
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class TestData {
+        private Integer numberField;
+        private String stringField;
+        private Map<String, Object> mapField;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class OtherTestData {
+        private Integer numberField;
+        private String stringField;
+        private Map<String, Object> mapField;
+    }
+}
