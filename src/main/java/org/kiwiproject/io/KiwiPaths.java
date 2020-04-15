18407c342d2e24d96eeeeba1c8d3f0d53b247c31
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-04-15 14:06:18 -0400

    Added in KiwiPaths and associated tests (#94)
    * Added in KiwiPaths and associated tests
    
    Fixes #91

---
+++ src/main/java/org/kiwiproject/io/KiwiPaths.java
@@ -0,0 +1,36 @@
+package org.kiwiproject.io;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.io.Resources;
+import lombok.experimental.UtilityClass;
+import org.kiwiproject.net.UncheckedURISyntaxException;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Static utilities related to {@link Path} instances.
+ */
+@UtilityClass
+public class KiwiPaths {
+
+    /**
+     * This wraps Guava's {@link Resources#getResource(String)} and converts it to a {@link Path}
+     *
+     * @see Resources#getResource(String)
+     */
+    public static Path pathFromResourceName(String resourceName) {
+        try {
+            return Paths.get(Resources.getResource(resourceName).toURI());
+        } catch (URISyntaxException e) {
+            throw newUncheckedException(resourceName, e);
+        }
+    }
+
+    @VisibleForTesting
+    static UncheckedURISyntaxException newUncheckedException(String resourceName, URISyntaxException e) {
+        var message = "Cannot convert to URI: " + resourceName;
+        return new UncheckedURISyntaxException(message, e);
+    }
+}
---
+++ src/test/java/org/kiwiproject/io/KiwiPathsTest.java
@@ -0,0 +1,39 @@
+package org.kiwiproject.io;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+@DisplayName("KiwiPaths")
+class KiwiPathsTest {
+
+    @Test
+    void testPathFromFileName_WhenValidRelativeFileName() {
+        var path = KiwiPaths.pathFromResourceName("logback.xml");
+
+        assertThat(path.toFile()).exists();
+        assertThat(path.toString()).endsWith("/logback.xml");
+    }
+
+    @Test
+    void testPathFromFileName_WhenFileDoesNotExist() {
+        assertThatThrownBy(() -> KiwiPaths.pathFromResourceName("does-not-exist"))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testInternalCreateAndThrowUncheckedException() {
+        var resourceName = "foo.txt";
+        var syntaxException = new URISyntaxException(resourceName, "the reason");
+
+        var unchecked = KiwiPaths.newUncheckedException(resourceName, syntaxException);
+
+        assertThat(unchecked)
+                .hasMessage("Cannot convert to URI: " + resourceName)
+                .hasCause(syntaxException);
+    }
+}
