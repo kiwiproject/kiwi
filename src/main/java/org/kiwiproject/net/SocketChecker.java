dd49b1f32aeb0c89488fba9ac7c0d1748eb18935
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-04-17 12:04:01 -0400

    Add SocketChecker and accompanying tests (#117)
    * Add SocketChecker and accompanying tests
    
    Fixes #112

---
+++ src/main/java/org/kiwiproject/net/SocketChecker.java
@@ -0,0 +1,57 @@
+package org.kiwiproject.net;
+
+import com.google.common.primitives.Ints;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.time.Duration;
+
+/**
+ * Utilities for checking sockets.
+ */
+@Slf4j
+public class SocketChecker {
+
+    public static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(5);
+
+    /**
+     * Check whether a {@link Socket} can be opened on the specified host and port.
+     */
+    public boolean canConnectViaSocket(String host, int port) {
+        return canConnectViaSocket(host, port, DEFAULT_TIMEOUT);
+    }
+
+    /**
+     * Check whether a {@link Socket} can be opened on the specified host and port, with the specified timeout.
+     */
+    public boolean canConnectViaSocket(String host, int port, Duration timeout) {
+        try (var socket = new Socket()) {
+            LOG.trace("Checking socket at {}:{} with timeout {} ms", host, port, timeout.toMillis());
+            socket.connect(new InetSocketAddress(host, port), Ints.checkedCast(timeout.toMillis()));
+            LOG.trace("Socket at {}:{} is available", host, port);
+            return true;
+        } catch (IOException e) {
+            LOG.trace("Socket at {}:{} not available", host, port, e);
+            return false;
+        }
+    }
+
+    /**
+     * Check whether a {@link Socket} can be opened on the specified host and port.
+     */
+    public boolean canConnectViaSocket(Pair<String, Integer> hostAndPort) {
+        return canConnectViaSocket(hostAndPort, DEFAULT_TIMEOUT);
+    }
+
+    /**
+     * Check whether a {@link Socket} can be opened on the specified host and port, with the specified timeout.
+     */
+    public boolean canConnectViaSocket(Pair<String, Integer> hostAndPort, Duration timeout) {
+        var host = hostAndPort.getLeft();
+        var port = hostAndPort.getRight();
+        return canConnectViaSocket(host, port, timeout);
+    }
+}
---
+++ src/test/java/org/kiwiproject/net/SocketCheckerTest.java
@@ -0,0 +1,94 @@
+package org.kiwiproject.net;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.awaitility.Awaitility.await;
+import static org.kiwiproject.net.LocalPortChecker.MAX_PORT;
+
+import lombok.Getter;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.ServerSocket;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
+
+@DisplayName("SocketChecker")
+class SocketCheckerTest {
+
+    private SocketChecker socketChecker;
+    private LocalPortChecker portChecker;
+
+    @BeforeEach
+    void setUp() {
+        socketChecker = new SocketChecker();
+        portChecker = new LocalPortChecker();
+    }
+
+    @Test
+    void testCanConnectViaSocket_WhenCanConnect_WithDefaultTimeout() throws InterruptedException {
+        var port = portChecker.findFirstOpenPortAbove(20_000)
+                .orElseThrow(() -> new IllegalStateException("Cannot find any open ports"));
+
+        var server = new StupidSimpleServer();
+        server.runAcceptOnlyOnceServer(port);
+        await().atMost(200, MILLISECONDS).until(() -> server.getRunningFlag().get());
+
+        assertThat(socketChecker.canConnectViaSocket("localhost", port)).isTrue();
+    }
+
+    @Test
+    void testCanConnectViaSocket_WhenCanConnect_AndMightOrMightNotTimeOut() throws InterruptedException {
+        var port = portChecker.findFirstOpenPortAbove(25_000)
+                .orElseThrow(() -> new IllegalStateException("Cannot find any open ports"));
+
+        var server = new StupidSimpleServer();
+        server.runAcceptOnlyOnceServer(port);
+
+        assertThatCode(() -> socketChecker.canConnectViaSocket(Pair.of("localhost", port), Duration.ofMillis(1)))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void testCanConnectViaSocket_WhenCannotConnect() {
+        var closedPort = findFirstClosedPort();
+        assertThat(socketChecker.canConnectViaSocket(Pair.of("localhost", closedPort))).isFalse();
+    }
+
+    private int findFirstClosedPort() {
+        return IntStream.rangeClosed(1024, MAX_PORT)
+                .filter(portChecker::isPortAvailable)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Did not find any closed ports"));
+    }
+
+    private static class StupidSimpleServer {
+
+        @Getter
+        AtomicBoolean runningFlag = new AtomicBoolean(false);
+
+        void runAcceptOnlyOnceServer(int port) throws InterruptedException {
+            var server = new Thread(() -> {
+                runningFlag.set(true);
+                try (var serverSocket = new ServerSocket(port)) {
+                    serverSocket.accept();
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+
+            server.setName("stupid-simple-server-thread");
+            server.start();
+
+            while (!runningFlag.get()) {
+                MILLISECONDS.sleep(10);
+            }
+        }
+    }
+}
