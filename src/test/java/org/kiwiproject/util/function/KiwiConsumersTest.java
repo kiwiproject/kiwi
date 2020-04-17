02ee1528282e4e00d908735c11c41f14eadc049c
Scott Leberknight <scottleber@gmail.com>
2020-04-17 18:48:43 -0400

    Add KiwiConsumers
    Fixes #125

---
+++ src/test/java/org/kiwiproject/util/function/KiwiConsumersTest.java
@@ -0,0 +1,47 @@
+package org.kiwiproject.util.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.Getter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.kiwiproject.base.UUIDs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+@DisplayName("KiwiConsumers")
+class KiwiConsumersTest {
+
+    @Test
+    void shouldProvideNoOpConsumer() {
+        var scaffold = new ConsumerNoOpScaffold();
+        var originalText = List.copyOf(scaffold.getText());
+        assertThat(originalText).hasSize(1);
+
+        scaffold.mutateValue(KiwiConsumers.noOp());
+        assertThat(scaffold.getText()).isEqualTo(originalText);
+
+        scaffold.mutateValue();
+        assertThat(scaffold.getText()).hasSize(2).isNotEqualTo(originalText);
+    }
+
+    static class ConsumerNoOpScaffold {
+
+        @Getter
+        private List<String> text = new ArrayList<>();
+
+        ConsumerNoOpScaffold() {
+            mutateValue();
+        }
+
+        void mutateValue() {
+            mutateValue(text -> text.add(UUIDs.randomUUIDString()));
+        }
+
+        void mutateValue(Consumer<List<String>> block) {
+            block.accept(text);
+        }
+    }
+}
