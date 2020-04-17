94b92331a1d7793e3a3af79dfa74527a8aa1bab9
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-04-17 13:24:57 -0400

    Add JacksonDataFormat enum (#121)
    Fixes #115

---
+++ src/main/java/org/kiwiproject/jackson/JacksonDataFormat.java
@@ -0,0 +1,52 @@
+package org.kiwiproject.jackson;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.Getter;
+
+/**
+ * Represents the detected type of a {@link String}.
+ * <p>
+ * Currently only supports JSON, XML, and Yaml formats.
+ *
+ * @implNote Uses Jackson under the covers. You will need to ensure that jackson-dataformat-xml and
+ * jackson-dataformat-yaml dependencies are present at runtime.
+ * @see com.fasterxml.jackson.core.format.DataFormatDetector
+ */
+public enum JacksonDataFormat {
+
+    JSON(JsonFactory.FORMAT_NAME_JSON),
+    XML(XmlFactory.FORMAT_NAME_XML),
+    YAML(YAMLFactory.FORMAT_NAME_YAML),
+    UNKNOWN("UNKNOWN");
+
+    @Getter
+    private final String formatName;
+
+    JacksonDataFormat(String formatName) {
+        this.formatName = formatName;
+    }
+
+    /**
+     * Returns one of the enum constants for the given {@code formatName}, or {@link #UNKNOWN} if the format name is
+     * not found.
+     * <p>
+     * The {@code formatName} <em>must match exactly</em> (i.e. is case-sensitive).
+     *
+     * @implNote Format names are the values of the public constants in the appropriate Jackson {@link JsonFactory}
+     * and its subclasses.
+     * @see #getFormatName()
+     */
+    public static JacksonDataFormat from(String formatName) {
+        if (JSON.formatName.equals(formatName)) {
+            return JSON;
+        } else if (XML.formatName.equals(formatName)) {
+            return XML;
+        } else if (YAML.formatName.equals(formatName)) {
+            return YAML;
+        }
+
+        return UNKNOWN;
+    }
+}
