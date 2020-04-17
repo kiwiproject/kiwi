19cdb3dfac2e92462a4b284821aa6cd06d9cea77
Scott Leberknight <scottleber@gmail.com>
2020-04-17 19:05:06 -0400

    Add KiwiBiConsumers
    Fixes #124

---
+++ src/main/java/org/kiwiproject/util/function/KiwiBiConsumers.java
@@ -0,0 +1,25 @@
+package org.kiwiproject.util.function;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.function.BiConsumer;
+
+/**
+ * Contains helper methods for {@link BiConsumer}.
+ */
+@UtilityClass
+public class KiwiBiConsumers {
+
+    /**
+     * Provides a type-consistent, no-op {@link BiConsumer}.
+     *
+     * @param <T> first type parameter of the BiConsumer
+     * @param <U> second  type parameter of the BiConsumer
+     * @return the no-op BiConsumer
+     */
+    public static <T, U> BiConsumer<T, U> noOp() {
+        return (t, u) -> {
+            // no-op
+        };
+    }
+}
