a8aae4996059dab6b0c634af466a1fefe1b7cdeb
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-04-27 18:45:01 -0400

    Add the generic PaginatedResult and Sorted interfaces (#147)
    Note these don't have implementations in the search
    package as of now. (We do have implementations elsewhere
    that we might want to add as reference implementations).
    
    Fixes #145
    Fixes #146

---
+++ src/main/java/org/kiwiproject/search/Sorted.java
@@ -0,0 +1,13 @@
+package org.kiwiproject.search;
+
+/**
+ * Simple contract for search results that are sorted in a specific direction.
+ */
+public interface Sorted {
+
+    /**
+     * Defines a general sort direction, which will usually by something like "asc" or "desc" but can be customized
+     * however an application likes.
+     */
+    String getSortDirection();
+}
