1943bdb15f7f6216ac2dce7825c7bfff61d8cc76
Scott Leberknight <scottleber@gmail.com>
2020-04-27 17:46:03 -0400

    Add the generic PaginatedResult and Sorted interfaces
    Note these don't have implementations in the search
    package as of now. (We do have implementations elsewhere
    that we might want to add as reference implementations).
    
    Fixes #145
    Fixes #146

---
+++ src/main/java/org/kiwiproject/search/PaginatedResult.java
@@ -0,0 +1,23 @@
+package org.kiwiproject.search;
+
+/**
+ * Simple interface defining basic pagination for any kind of search, e.g. a database query, SOLR search, etc.
+ */
+public interface PaginatedResult {
+
+    /**
+     * The total number of results, though a {@link PaginatedResult} instance will contain only a subset of the total.
+     */
+    long getTotalCount();
+
+    /**
+     * The page number of this result. Can be used in both zero- and one-based page numbering schemes as long
+     * as it is used consistently, e.g. the server and clients are both using one-based page numbering.
+     */
+    int getPageNumber();
+
+    /**
+     * The page size being used to paginate the search results.
+     */
+    int getPageSize();
+}
