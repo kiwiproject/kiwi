a67ad3c505816b7a491f5a45cfdd69916cd6d05c
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-04-28 10:47:38 -0400

    Add MatchSpliterator (#153)
    Fixes #148

---
+++ src/main/java/org/kiwiproject/regex/MatchSpliterator.java
@@ -0,0 +1,105 @@
+package org.kiwiproject.regex;
+
+import static org.kiwiproject.base.KiwiPreconditions.checkArgumentNotBlank;
+import static org.kiwiproject.base.KiwiPreconditions.checkArgumentNotNull;
+import static org.kiwiproject.base.KiwiPreconditions.requireNotNull;
+
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * This is a {@link java.util.Spliterator} that lets you traverse {@link MatchResult}s of a {@link Matcher}.
+ * Generally you should use one of the {@code stream} methods, but if you need a {@link java.util.Spliterator}
+ * instance you can use the constructor directly.
+ * <p>
+ * Gratefully included here in Kiwi courtesy of Philipp Wagner from his "A Spliterator for MatchResults in Java"
+ * blog entry. Slightly modified to check arguments and add an instance {@code stream} method.
+ * <p>
+ * Original blog post: https://bytefish.de/blog/matcher_spliterator/
+ */
+public class MatchSpliterator extends Spliterators.AbstractSpliterator<MatchResult> {
+
+    private static final boolean PARALLEL_FALSE = false;
+
+    private final Matcher matcher;
+
+    /**
+     * Create a new instance from the given {@link Matcher}. Usually you won't use this directly. Instead use the
+     * {@code stream} methods.
+     */
+    public MatchSpliterator(Matcher matcher) {
+        super(Long.MAX_VALUE, NONNULL | ORDERED);
+
+        this.matcher = requireNotNull(matcher);
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super MatchResult> action) {
+        if (matcher.find()) {
+            action.accept(matcher.toMatchResult());
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Create a  {@link Stream} of {@link MatchResult} from this instance.
+     *
+     * @return a stream of match results
+     */
+    public Stream<MatchResult> stream() {
+        return stream(matcher);
+    }
+
+    /**
+     * Create a {@link Stream} of {@link MatchResult} using the given regular expression and String input.
+     * <p>
+     * <em>This method will compile a new {@link Pattern} every time it is called, even if the same regular
+     * expression is supplied.</em> Compiling regular expressions is considered an "expensive" operation.
+     * Therefore you should generally prefer {@link #stream(Pattern, String)} since you can precompile a
+     * {@link Pattern} and re-use it many times.
+     *
+     * @param regex the regular expression as a String
+     * @param input the input to match against
+     * @return a stream of match results
+     * @throws IllegalArgumentException if the regex is blank
+     */
+    public static Stream<MatchResult> stream(String regex, String input) {
+        checkArgumentNotBlank(regex);
+
+        return stream(Pattern.compile(regex), input);
+    }
+
+    /**
+     * Create a {@link Stream} of {@link MatchResult} using the given {@link Pattern} and String input.
+     *
+     * @param pattern the {@link Pattern} to use when matching the input
+     * @param input   the input to match against
+     * @return a stream of match results
+     * @throws IllegalArgumentException if the pattern is null
+     */
+    public static Stream<MatchResult> stream(Pattern pattern, String input) {
+        checkArgumentNotNull(pattern);
+
+        return stream(pattern.matcher(input));
+    }
+
+    /**
+     * Create a {@link Stream} of {@link MatchResult} using the given {@link Matcher}
+     *
+     * @param matcher the {@link Matcher} to use
+     * @return a stream of match results
+     * @throws IllegalArgumentException if the matcher is null
+     */
+    public static Stream<MatchResult> stream(Matcher matcher) {
+        checkArgumentNotNull(matcher);
+
+        return StreamSupport.stream(new MatchSpliterator(matcher), PARALLEL_FALSE);
+    }
+}
---
+++ src/test/java/org/kiwiproject/regex/MatchSpliteratorTest.java
@@ -0,0 +1,138 @@
+package org.kiwiproject.regex;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.tuple;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.ArrayList;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+@DisplayName("MatchSpliterator")
+class MatchSpliteratorTest {
+
+    @Nested
+    class Constructor {
+
+        @Test
+        void shouldConstructFromMatcher() {
+            var input = "The quick brown fox jumped over the fence and the lazy brown dog";
+            var matcher = Pattern.compile("the").matcher(input);
+            var matchSpliterator = new MatchSpliterator(matcher);
+
+            var startIndexes = new ArrayList<Integer>();
+            matchSpliterator.forEachRemaining(matchResult -> startIndexes.add(matchResult.start()));
+
+            assertThat(startIndexes).containsExactly(32, 46);
+        }
+
+        @Test
+        void shouldNotAllowNullMatcher() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new MatchSpliterator(null));
+        }
+    }
+
+    @Nested
+    class StreamInstanceMethod {
+
+        @Test
+        void shouldCreateMatchResultStream() {
+            var input = "The quick brown fox jumped over the fence and the lazy brown dog";
+            var matcher = Pattern.compile("the|(lazy cat)", Pattern.CASE_INSENSITIVE).matcher(input);
+            var matchSpliterator = new MatchSpliterator(matcher);
+
+            var matchResults = matchSpliterator.stream().collect(toList());
+            assertThat(matchResults)
+                    .extracting(MatchResult::group, MatchResult::start)
+                    .containsExactly(
+                            tuple("The", 0),
+                            tuple("the", 32),
+                            tuple("the", 46)
+                    );
+        }
+    }
+
+    @Nested
+    class StreamFromRegex {
+
+        @Test
+        void shouldCreateMatchResultStream_FromStringRegularExpression() {
+            var input = "The quick brown fox jumped over the fence and the lazy brown dog";
+            var matchResults = MatchSpliterator.stream("the", input).collect(toList());
+
+            assertThat(matchResults)
+                    .extracting(MatchResult::group, MatchResult::start)
+                    .containsExactly(
+                            tuple("the", 32),
+                            tuple("the", 46)
+                    );
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldNotAllowBlankRegex(String regex) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> MatchSpliterator.stream(regex, "the input"));
+        }
+    }
+
+    @Nested
+    class StreamFromPattern {
+
+        @Test
+        void shouldCreateMatchResultStream_FromPattern() {
+            var input = "The quick brown fox jumped over the fence and the lazy brown dog and the lazy cat";
+            var pattern = Pattern.compile("the|(lazy cat)", Pattern.CASE_INSENSITIVE);
+            var matchResults = MatchSpliterator.stream(pattern, input).collect(toList());
+
+            assertThat(matchResults)
+                    .extracting(MatchResult::group, MatchResult::start)
+                    .containsExactly(
+                            tuple("The", 0),
+                            tuple("the", 32),
+                            tuple("the", 46),
+                            tuple("the", 69),
+                            tuple("lazy cat", 73)
+                    );
+        }
+
+        @Test
+        void shouldNotAllowNullPattern() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> MatchSpliterator.stream((Pattern) null, "the input"));
+        }
+    }
+
+    @Nested
+    class StreamFromMatcher {
+
+        @Test
+        void shouldCreateMatchResultStream_FromMatcher() {
+            var pattern = Pattern.compile("(\\d{1,3}).(\\d{1,3}).(\\d{1,3}).(\\d{1,3})");
+            var matcher = pattern.matcher("192.168.1.108  192.168.1.107  192.168.5.102  192.168.6.112");
+            var matchResults = MatchSpliterator.stream(matcher);
+
+            assertThat(matchResults)
+                    .extracting(MatchResult::group)
+                    .containsExactly(
+                            "192.168.1.108",
+                            "192.168.1.107",
+                            "192.168.5.102",
+                            "192.168.6.112");
+        }
+
+        @Test
+        void shouldNotAllowNullMatcher() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> MatchSpliterator.stream(null));
+        }
+    }
+}
