d319637377f9132ca4d90db234279a3b267da653
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-04-30 12:24:15 -0400

    Add KiwiDates (#177)
    Fixes #169

---
+++ src/main/java/org/kiwiproject/time/KiwiDates.java
@@ -0,0 +1,155 @@
+package org.kiwiproject.time;
+
+import static org.kiwiproject.time.KiwiInstant.minusDays;
+import static org.kiwiproject.time.KiwiInstant.minusHours;
+import static org.kiwiproject.time.KiwiInstant.minusMinutes;
+import static org.kiwiproject.time.KiwiInstant.minusMonths;
+import static org.kiwiproject.time.KiwiInstant.minusYears;
+import static org.kiwiproject.time.KiwiInstant.plusDays;
+import static org.kiwiproject.time.KiwiInstant.plusHours;
+import static org.kiwiproject.time.KiwiInstant.plusMinutes;
+import static org.kiwiproject.time.KiwiInstant.plusMonths;
+import static org.kiwiproject.time.KiwiInstant.plusYears;
+
+import lombok.experimental.UtilityClass;
+
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * The original JDK 1.0 {@link Date} class leaves, ahem, much to be desired, but there are still (unfortunately)
+ * many classes, interfaces, and libraries that use it and probably won't be changed soon or ever. KiwiDate contains
+ * some utilities to convert {@link Instant} objects to {@link Date} objects shifted by some amount of time, for
+ * example if you need a Date that is 30 minutes from a given {@link Instant}.
+ * <p>
+ * If you have a choice, you should always prefer the Java 8 date/time APIs (e.g. {@link Instant},
+ * {@link java.time.LocalDateTime}, {@link java.time.ZonedDateTime}) in the {@code java.time} package
+ * over using the legacy {@link Date}. But if you need to convert instants to dates, then this will help.
+ * <p>
+ * Functionally this is just a Date wrapper for {@link KiwiInstant}.
+ */
+@UtilityClass
+public class KiwiDates {
+
+    /**
+     * Return the {@link Date} converted from {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @return the converted {@link Date}
+     * @apiNote This is just a simple wrapper around {@link Date#from(Instant)} and is mainly here for consistency
+     * with the other utilities provided here
+     */
+    public static Date dateFrom(Instant instant) {
+        return Date.from(instant);
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code minutes} before the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param minutes number of minutes to subtract from {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantMinusMinutes(Instant instant, long minutes) {
+        return dateFrom(minusMinutes(instant, minutes));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code hours} before the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param hours   number of hours to subtract from {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantMinusHours(Instant instant, long hours) {
+        return dateFrom(minusHours(instant, hours));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code days} before the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param days    number of days to subtract from {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantMinusDays(Instant instant, long days) {
+        return dateFrom(minusDays(instant, days));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code months} before the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param months  number of months to subtract from {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantMinusMonths(Instant instant, long months) {
+        return dateFrom(minusMonths(instant, months));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code years} before the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param years   number of years to subtract from {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantMinusYears(Instant instant, long years) {
+        return dateFrom(minusYears(instant, years));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code minutes} after the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param minutes number of minutes to add to {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantPlusMinutes(Instant instant, long minutes) {
+        return dateFrom(plusMinutes(instant, minutes));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code hours} after the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param hours   number of hours to add to {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantPlusHours(Instant instant, long hours) {
+        return dateFrom(plusHours(instant, hours));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code days} after the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param days    number of days to add to {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantPlusDays(Instant instant, long days) {
+        return dateFrom(plusDays(instant, days));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code months} after the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param months  number of months to add to {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantPlusMonths(Instant instant, long months) {
+        return dateFrom(plusMonths(instant, months));
+    }
+
+    /**
+     * Return the {@link Date} that is the given number of {@code years} after the given {@code instant}.
+     *
+     * @param instant the {@link Instant} to convert
+     * @param years   number of years to add to {@code instant}
+     * @return the converted {@link Date}
+     */
+    public static Date dateFromInstantPlusYears(Instant instant, long years) {
+        return dateFrom(plusYears(instant, years));
+    }
+}
---
+++ src/test/java/org/kiwiproject/time/KiwiDatesTest.java
@@ -0,0 +1,168 @@
+package org.kiwiproject.time;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+@DisplayName("KiwiDate")
+class KiwiDatesTest {
+
+    private Instant now;
+
+    @BeforeEach
+    void setUp() {
+        now = Instant.now();
+    }
+
+    @Nested
+    class DateFrom {
+
+        @Test
+        void shouldConvertInstantToDate() {
+            assertThat(KiwiDates.dateFrom(now)).isEqualTo(Date.from(now));
+        }
+    }
+
+    @Nested
+    class MinuteAdjusters {
+
+        private Instant aMinuteAgo;
+
+        @BeforeEach
+        void setUp() {
+            aMinuteAgo = now.minus(1, ChronoUnit.MINUTES);
+        }
+
+        @Test
+        void shouldAddMinutes() {
+            assertThat(KiwiDates.dateFromInstantPlusMinutes(aMinuteAgo, 5))
+                    .isEqualTo(Date.from(now.plus(4, ChronoUnit.MINUTES)));
+        }
+
+        @Test
+        void shouldSubtractMinutes() {
+            assertThat(KiwiDates.dateFromInstantMinusMinutes(aMinuteAgo, 5))
+                    .isEqualTo(Date.from(now.minus(6, ChronoUnit.MINUTES)));
+        }
+    }
+
+    @Nested
+    class HourAdjusters {
+
+        private Instant anHourAgo;
+
+        @BeforeEach
+        void setUp() {
+            anHourAgo = now.minus(1, ChronoUnit.HOURS);
+        }
+
+        @Test
+        void shouldAddHours() {
+            assertThat(KiwiDates.dateFromInstantPlusHours(anHourAgo, 2))
+                    .isEqualTo(Date.from(now.plus(1, ChronoUnit.HOURS)));
+        }
+
+        @Test
+        void shouldSubtractHours() {
+            assertThat(KiwiDates.dateFromInstantMinusHours(anHourAgo, 2))
+                    .isEqualTo(Date.from(now.minus(3, ChronoUnit.HOURS)));
+        }
+    }
+
+    @Nested
+    class DayAdjusters {
+
+        private Instant aDayAgo;
+
+        @BeforeEach
+        void setUp() {
+            aDayAgo = now.minus(1, ChronoUnit.DAYS);
+        }
+
+        @Test
+        void shouldAddDays() {
+            assertThat(KiwiDates.dateFromInstantPlusDays(aDayAgo, 2))
+                    .isEqualTo(Date.from(now.plus(1, ChronoUnit.DAYS)));
+        }
+
+        @Test
+        void shouldSubtractDays() {
+            assertThat(KiwiDates.dateFromInstantMinusDays(aDayAgo, 2))
+                    .isEqualTo(Date.from(now.minus(3, ChronoUnit.DAYS)));
+        }
+    }
+
+    /**
+     * @implNote Because {@link Instant} does not deal with {@link TemporalUnit} values greater than
+     * {@link ChronoUnit#DAYS}, these tests use {@link ZonedDateTime} to adjust the time scales.
+     */
+    @Nested
+    class MonthAdjusters {
+
+        private ZonedDateTime zdtNow;
+        private Instant aMonthAgo;
+
+        @BeforeEach
+        void setUp() {
+            zdtNow = ZonedDateTime.ofInstant(now, ZoneOffset.UTC);
+
+            // Adjustment to handle months having different lengths (so tests pass at end of month)
+            while (zdtNow.getDayOfMonth() > 28) {
+                zdtNow = zdtNow.minusDays(1);
+            }
+
+            aMonthAgo = zdtNow.minusMonths(1).toInstant();
+        }
+
+        @Test
+        void shouldAddMonths() {
+            assertThat(KiwiDates.dateFromInstantPlusMonths(aMonthAgo, 2))
+                    .isEqualTo(Date.from(zdtNow.plusMonths(1).toInstant()));
+        }
+
+        @Test
+        void shouldSubtractMonths() {
+            assertThat(KiwiDates.dateFromInstantMinusMonths(aMonthAgo, 2))
+                    .isEqualTo(Date.from(zdtNow.minusMonths(3).toInstant()));
+        }
+    }
+
+    /**
+     * @implNote Because {@link Instant} does not deal with {@link TemporalUnit} values greater than
+     * {@link ChronoUnit#DAYS}, these tests use {@link ZonedDateTime} to adjust the time scales.
+     */
+    @Nested
+    class YearsAdjusters {
+
+        private ZonedDateTime zdtNow;
+        private Instant aYearAgo;
+
+        @BeforeEach
+        void setUp() {
+            zdtNow = ZonedDateTime.ofInstant(now, ZoneOffset.UTC);
+            aYearAgo = zdtNow.minusYears(1).toInstant();
+        }
+
+        @Test
+        void shouldAddYears() {
+            assertThat(KiwiDates.dateFromInstantPlusYears(aYearAgo, 2))
+                    .isEqualTo(Date.from(zdtNow.plusYears(1).toInstant()));
+        }
+
+        @Test
+        void shouldSubtractYears() {
+            assertThat(KiwiDates.dateFromInstantMinusYears(aYearAgo, 2))
+                    .isEqualTo(Date.from(zdtNow.minusYears(3).toInstant()));
+        }
+    }
+}
