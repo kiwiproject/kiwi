e84482658058f62a6de98f1162bc905e2c929f90
Scott Leberknight <scottleber@gmail.com>
2020-06-10 17:47:49 -0400

    Sonar: remove redundant public modifier from KillSignalTest

--- src/test/java/org/kiwiproject/base/process/KillSignalTest.java
+++ src/test/java/org/kiwiproject/base/process/KillSignalTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("KillSignal")
-public class KillSignalTest {
+class KillSignalTest {
 
     @ParameterizedTest
     @CsvSource({
@@ -18,27 +18,27 @@ public class KillSignalTest {
             "SIGKILL,9",
             "SIGTERM,15",
     })
-    public void testSignalNumbers(String killSignalName, String expectedNumber) {
+    void testSignalNumbers(String killSignalName, String expectedNumber) {
         assertThat(KillSignal.valueOf(killSignalName).number()).isEqualTo(expectedNumber);
     }
 
     @Test
-    public void testStaticWithLeadingDash_WhenHasLeadingDash() {
+    void testStaticWithLeadingDash_WhenHasLeadingDash() {
         assertThat(KillSignal.withLeadingDash("-15")).isEqualTo("-15");
     }
 
     @Test
-    public void testStaticWithLeadingDash_WhenDoesNotHaveLeadingDash() {
+    void testStaticWithLeadingDash_WhenDoesNotHaveLeadingDash() {
         assertThat(KillSignal.withLeadingDash("9")).isEqualTo("-9");
     }
 
     @Test
-    public void testWithLeadingDash_WhenHasLeadingDash() {
+    void testWithLeadingDash_WhenHasLeadingDash() {
         assertThat(KillSignal.SIGHUP.withLeadingDash()).isEqualTo("-1");
     }
 
     @Test
-    public void testWithLeadingDash_WhenDoesNotHaveLeadingDash() {
+    void testWithLeadingDash_WhenDoesNotHaveLeadingDash() {
         assertThat(KillSignal.SIGQUIT.withLeadingDash()).isEqualTo("-3");
     }
 
