d0eabecba0b20f6fe48ef3ba0dd711dc9916dd5e
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-08-07 16:18:38 -0400

    Add XML exception classes (#286)
    * Add UncheckedJAXBException, an unchecked wrapper for JAXBExceptions
    * Add XmlRuntimeException, for wrapping various & sundry checked
      XML processing exceptions
    
    Fixes #259
    Fixes #261

--- src/test/java/org/kiwiproject/xml/XmlRuntimeExceptionTest.java
+++ src/test/java/org/kiwiproject/xml/XmlRuntimeExceptionTest.java
@@ -1,4 +1,4 @@
-package org.kiwiproject.yaml;
+package org.kiwiproject.xml;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-@DisplayName("RuntimeYamlException")
-class RuntimeYamlExceptionTest {
+@DisplayName("XmlRuntimeException")
+class XmlRuntimeExceptionTest {
 
     @Test
     void shouldAcceptMessage() {
-        assertThat(new RuntimeYamlException("oops"))
+        assertThat(new XmlRuntimeException("oops"))
                 .hasNoCause()
                 .hasMessage("oops");
     }
@@ -20,15 +20,15 @@ class RuntimeYamlExceptionTest {
     @Test
     void shouldAcceptMessageAndCause() {
         var cause = new IOException("oops");
-        assertThat(new RuntimeYamlException("bad YAML", cause))
-                .hasMessage("bad YAML")
+        assertThat(new XmlRuntimeException("bad Xml", cause))
+                .hasMessage("bad Xml")
                 .hasCauseReference(cause);
     }
 
     @Test
     void shouldAcceptCause() {
         var cause = new IOException("the cause");
-        assertThat(new RuntimeYamlException(cause))
+        assertThat(new XmlRuntimeException(cause))
                 .hasMessageContaining("the cause")
                 .hasCauseReference(cause);
     }
