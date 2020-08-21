81663c540055dc7fd7ea7365dbfaa5ab6406e2b1
Scott Leberknight <scottleber@gmail.com>
2020-08-21 17:57:10 -0400

    Add @Ipv4Address and @Ipv4AndPort validation annotations
    * Add @Ipv4Address validation annotation
    * Add Ipv4AddressValidator, the validator class for @Ipv4Address
    * Add @Ipv4AndPort validation annotation
    * Add Ipv4AndPortValidator, the validator class for @Ipv4AndPort
    
    Fixes #307
    Fixed #308

---
+++ src/main/java/org/kiwiproject/validation/Ipv4AndPort.java
@@ -0,0 +1,46 @@
+package org.kiwiproject.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * The annotated element must have the form {@code ipv4-address:port}. For example {@code 192.168.1.150:8888} is
+ * valid while {@code 192.168.1.150} (without port) and {@code 1111.1111.1.150:8009} (two many digits in octets) are
+ * not. Also, as the name indicates, this only supports IPv4 addresses.
+ *
+ * @implNote Currently the validation only verifies that the IP is in "dotted-quad" syntax followed by a colon and
+ * a port. It only validates that each dotted-quad and port are numbers, and <em>does not</em> validate the values
+ * are within valid ranges (i.e. it does not check each segment is in the range 0-255 nor does it validate the port
+ * range).
+ */
+@Documented
+@Constraint(validatedBy = {Ipv4AndPortValidator.class})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+public @interface Ipv4AndPort {
+
+    String message() default "{org.kiwiproject.validation.Ipv4AndPort.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Whether to consider null as valid. The default is false.
+     *
+     * @return true to consider null as valid
+     */
+    boolean allowNull() default false;
+
+}
---
+++ src/test/java/org/kiwiproject/validation/Ipv4AddressValidatorTest.java
@@ -0,0 +1,224 @@
+package org.kiwiproject.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kiwiproject.validation.ValidationTestHelper.assertNoPropertyViolations;
+import static org.kiwiproject.validation.ValidationTestHelper.assertNoViolations;
+import static org.kiwiproject.validation.ValidationTestHelper.assertPropertyViolations;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.validation.Validator;
+
+@DisplayName("Ipv4AddressValidator")
+class Ipv4AddressValidatorTest {
+
+    private static final String NOT_A_VALID_IPV_4_ADDRESS_MESSAGE = "is not a valid IPv4 address";
+    private static final int SENTINEL_INVALID_SEGMENT = -1;
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = KiwiValidations.getValidator();
+    }
+
+    @Nested
+    class WhenNullValue {
+
+        @Test
+        void shouldBeInvalid_ByDefault() {
+            var config = new Config(null);
+            assertPropertyViolations(validator, config, "ipAddress", NOT_A_VALID_IPV_4_ADDRESS_MESSAGE);
+        }
+
+        @Test
+        void shouldBeValid_WhenAllowingNulls() {
+            var config = new ConfigAllowingNull(null);
+            assertNoViolations(validator, config);
+        }
+    }
+
+    @Nested
+    class ShouldBeValid {
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "0.0.0.0",
+                "255.0.0.0",
+                "0.255.0.0",
+                "0.0.255.0",
+                "0.0.0.255",
+                "255.255.255.255",
+                "255.255.0.0",
+                "255.255.255.0",
+                "0.255.255.0",
+                "0.255.255.255",
+                "0.0.255.255",
+        })
+        void whenBoundaryConditionIPs(String ip) {
+            var config = new Config(ip);
+            assertNoPropertyViolations(validator, config, "ipAddress");
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "127.0.0.1",
+                "1.1.1.1",
+                "11.11.11.11",
+                "111.111.111.111",
+                "10.10.50.255",
+                "192.168.1.101",
+                "192.168.111.1",
+                "192.168.111.11",
+                "192.168.111.111",
+                "5.0.0.128",
+        })
+        void whenValidIps(String ip) {
+            var config = new Config(ip);
+            assertNoPropertyViolations(validator, config, "ipAddress");
+        }
+    }
+
+    @Nested
+    class ShouldBeInvalid {
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "",
+                " ",
+                "1",
+                "1.",
+                "1.1",
+                "1.1.",
+                "1.1.1",
+                "1.1.1.",
+                "256.1.1.1",
+                "1.256.1.1",
+                "1.1.256.1",
+                "1.1.1.256",
+                "256.256.256.256",
+                "-1.1.1.1",
+                "1.-1.1.1",
+                "1.1.-1.1",
+                "1.1.1.-1",
+                "a.1.1.1",
+                "1.a.1.1",
+                "1.1.a.1",
+                "1.1.1.a",
+                "a.b.c.d",
+        })
+        void whenTheIpsAreNotValid(String ip) {
+            var config = new Config(ip);
+            assertPropertyViolations(validator, config, "ipAddress", NOT_A_VALID_IPV_4_ADDRESS_MESSAGE);
+        }
+    }
+
+    @Nested
+    class IpSegmentAsInt {
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "0",
+                "1",
+                "192",
+                "255",
+                "256",
+        })
+        void shouldReturnInteger_WhenSegmentIsAnInteger(String segment) {
+            assertThat(Ipv4AddressValidator.ipSegmentAsInt(segment))
+                    .isEqualTo(Integer.parseInt(segment));
+        }
+
+        @Nested
+        class ShouldReturnSentinelInvalidSegment {
+
+            @ParameterizedTest
+            @ValueSource(strings = {
+                    "a",
+                    "ab",
+                    "abcd",
+            })
+            void whenSegmentIsNotAnInteger(String segment) {
+                assertThat(Ipv4AddressValidator.ipSegmentAsInt(segment)).isEqualTo(SENTINEL_INVALID_SEGMENT);
+            }
+
+            @ParameterizedTest
+            @NullAndEmptySource
+            void whenSegmentIsEmpty(String segment) {
+                assertThat(Ipv4AddressValidator.ipSegmentAsInt(segment)).isEqualTo(SENTINEL_INVALID_SEGMENT);
+            }
+
+            @ParameterizedTest
+            @ValueSource(strings = {
+                    " ",
+                    "  "
+            })
+            void whenSegmentIsBlank(String segment) {
+                assertThat(Ipv4AddressValidator.ipSegmentAsInt(segment)).isEqualTo(SENTINEL_INVALID_SEGMENT);
+            }
+
+            @ParameterizedTest
+            @ValueSource(strings = {
+                    "01",
+                    "010",
+                    "001",
+            })
+            void whenSegmentStartsWithZero(String segment) {
+                assertThat(Ipv4AddressValidator.ipSegmentAsInt(segment)).isEqualTo(SENTINEL_INVALID_SEGMENT);
+            }
+        }
+    }
+
+    @Nested
+    class IsInvalidIpSegment {
+
+        @ParameterizedTest
+        @CsvSource({
+                "0, false",
+                "1, false",
+                "192, false",
+                "255, false",
+                "-1, true",
+                "-2, true",
+                "-192, true",
+                "-2147483648, true",
+                "256, true",
+                "512, true",
+                "2147483647, true",
+        })
+        void shouldReturnExpectedResult(int ipSegment, boolean expectedResult) {
+            assertThat(Ipv4AddressValidator.isInvalidIpSegment(ipSegment)).isEqualTo(expectedResult);
+        }
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Config {
+
+        @Getter
+        @Setter
+        @Ipv4Address
+        private String ipAddress;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class ConfigAllowingNull {
+
+        @Getter
+        @Setter
+        @Ipv4Address(allowNull = true)
+        private String ipAddress;
+    }
+}
