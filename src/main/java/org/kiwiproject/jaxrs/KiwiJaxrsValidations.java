4521efb0a1256d75da9caa3324440e2df8dc099a
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-09-15 13:54:09 -0400

    Add KiwiJaxrsValidations (#362)
    * Add KiwiJaxrsValidations
    
    Fixes #354

---
+++ src/main/java/org/kiwiproject/jaxrs/KiwiJaxrsValidations.java
@@ -0,0 +1,150 @@
+package org.kiwiproject.jaxrs;
+
+import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.kiwiproject.collect.KiwiSets.isNotNullOrEmpty;
+import static org.kiwiproject.logging.LazyLogParameterSupplier.lazy;
+
+import com.google.common.annotations.VisibleForTesting;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import org.kiwiproject.jaxrs.exception.JaxrsBadRequestException;
+import org.kiwiproject.jaxrs.exception.JaxrsValidationException;
+import org.kiwiproject.validation.KiwiConstraintViolations;
+import org.kiwiproject.validation.KiwiValidations;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.groups.Default;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Static utility methods that perform validation on an object or value, and throw an appropriate
+ * subclass of {@link org.kiwiproject.jaxrs.exception.JaxrsException JaxrsException} if there are validation errors.
+ */
+@UtilityClass
+@Slf4j
+public class KiwiJaxrsValidations {
+
+    @VisibleForTesting
+    static final String MISSING_VALUE_MESSAGE = "Missing required value";
+
+    /**
+     * Assert that the given object is valid using the default validation group.
+     *
+     * @param id  the unique identity or ID of the object being validated
+     * @param obj the object to validate
+     * @param <T> the type of object
+     * @throws JaxrsValidationException if there are any validation errors
+     * @see Default
+     */
+    public static <T> void assertValid(String id, T obj) {
+        assertValid(id, obj, Default.class);
+    }
+
+    /**
+     * Assert that the given object is valid using the specified validation group classes.
+     * <p>
+     * Note that if you want the default validation group included, it must be in {@code groupClasses}.
+     *
+     * @param id           the unique identity or ID of the object being validated
+     * @param obj          the object to validate
+     * @param groupClasses validation groups to apply during validation
+     * @param <T>          the type of object
+     * @throws JaxrsValidationException if there are any validation errors
+     */
+    public static <T> void assertValid(String id, T obj, Class<?>... groupClasses) {
+        var violations = KiwiValidations.validate(obj, groupClasses);
+        if (isNotNullOrEmpty(violations)) {
+            debugLogValidationErrors(id, obj, violations);
+            throw new JaxrsValidationException(id, violations);
+        }
+    }
+
+    /**
+     * Assert that the given object is valid using the default validation group and the specified property path
+     * map, which allows callers to translate property paths (e.g. firstName) to a human-readable
+     * names (e.g. First Name).
+     *
+     * @param id                   the unique identity or ID of the object being validated
+     * @param obj                  the object to validate
+     * @param propertyPathMappings mapping from property path to (human-readable) field name
+     * @param <T>                  the type of object
+     * @throws JaxrsValidationException if there are any validation errors
+     * @see Default
+     */
+    public static <T> void assertValid(String id, T obj, Map<String, String> propertyPathMappings) {
+        assertValid(id, obj, propertyPathMappings, Default.class);
+    }
+
+    /**
+     * Assert that the given object is valid using the specified validation group classes and property path
+     * map.
+     * <p>
+     * Note that if you want the default validation group included, it must be in {@code groupClasses}.
+     *
+     * @param id                   the unique identity or ID of the object being validated
+     * @param obj                  the object to validate
+     * @param propertyPathMappings mapping from property path to (human-readable) field name
+     * @param groupClasses         validation groups to apply during validation
+     * @param <T>                  the type of object
+     * @throws JaxrsValidationException if there are any validation errors
+     */
+    public static <T> void assertValid(String id,
+                                       T obj,
+                                       Map<String, String> propertyPathMappings,
+                                       Class<?>... groupClasses) {
+        var violations = KiwiValidations.validate(obj, groupClasses);
+        if (isNotNullOrEmpty(violations)) {
+            debugLogValidationErrors(id, obj, violations);
+            throw new JaxrsValidationException(id, violations, propertyPathMappings);
+        }
+    }
+
+    private static <T> void debugLogValidationErrors(String id, T obj, Set<ConstraintViolation<T>> violations) {
+        LOG.debug("Encountered validation errors for {} (id: {}): {}",
+                obj.getClass().getSimpleName(),
+                id,
+                lazy(() -> KiwiConstraintViolations.simpleCombinedErrorMessage(violations)));
+    }
+
+    /**
+     * Assert that the given String value is not blank.
+     *
+     * @param fieldName the field name being validated
+     * @param value     the String value to check
+     * @throws JaxrsBadRequestException if the value is blank
+     * @implNote Blank check is performed using {@link org.apache.commons.lang3.StringUtils#isBlank(CharSequence)})
+     */
+    public static void assertNotBlank(String fieldName, String value) {
+        if (isBlank(value)) {
+            throw new JaxrsBadRequestException(MISSING_VALUE_MESSAGE, fieldName);
+        }
+    }
+
+    /**
+     * Assert that the given Object is not null.
+     *
+     * @param fieldName the field name being validated
+     * @param value     the value to check
+     * @throws JaxrsBadRequestException if the value is null
+     */
+    public static void assertNotNull(String fieldName, Object value) {
+        if (isNull(value)) {
+            throw new JaxrsBadRequestException(MISSING_VALUE_MESSAGE, fieldName);
+        }
+    }
+
+    /**
+     * Asserts that the given boolean value is true.
+     *
+     * @param value   the value to check
+     * @param message the message to use in the thrown exception
+     * @throws JaxrsBadRequestException if the value is false
+     */
+    public static void assertTrue(boolean value, String message) {
+        if (!value) {
+            throw new JaxrsBadRequestException(message);
+        }
+    }
+}
---
+++ src/test/java/org/kiwiproject/jaxrs/KiwiJaxrsValidationsTest.java
@@ -0,0 +1,239 @@
+package org.kiwiproject.jaxrs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import lombok.Builder;
+import lombok.Value;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.kiwiproject.jaxrs.exception.ErrorMessage;
+import org.kiwiproject.jaxrs.exception.JaxrsBadRequestException;
+import org.kiwiproject.jaxrs.exception.JaxrsValidationException;
+import org.kiwiproject.util.BlankStringArgumentsProvider;
+import org.kiwiproject.validation.group.ExistingObject;
+import org.kiwiproject.validation.group.KiwiValidationGroups;
+import org.kiwiproject.validation.group.NewObject;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.util.Map;
+
+@DisplayName("KiwiJaxrsValidations")
+class KiwiJaxrsValidationsTest {
+
+    private static final Map<String, String> PROPERTY_PATH_MAP = Map.of(
+            "id", "ID",
+            "emailAddress", "Email",
+            "mobileNumber", "Mobile phone"
+    );
+
+    @Nested
+    class AssertValid {
+
+        @Test
+        void shouldNotThrow_WhenNoValidationErrors() {
+            var contact = ContactInfo.builder()
+                    .emailAddress("alice@example.org")
+                    .mobileNumber("703-555-1212")
+                    .build();
+
+            assertThatCode(() -> KiwiJaxrsValidations.assertValid(null, contact))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void shouldThrow_WhenValidationErrorsExist_UsingDefaultValidationGroup() {
+            var contact = ContactInfo.builder()
+                    .emailAddress("alice")
+                    .build();
+
+            var thrown = catchThrowable(() -> KiwiJaxrsValidations.assertValid(null, contact));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsValidationException.class);
+            var validationException = (JaxrsValidationException) thrown;
+
+            var statusCode = 422;
+            assertThat(validationException.getStatusCode()).isEqualTo(statusCode);
+            assertThat(validationException.getOtherData()).isEmpty();
+            assertThat(validationException.getErrors()).containsExactlyInAnyOrder(
+                    new ErrorMessage(statusCode, "must be a well-formed email address", "emailAddress"),
+                    new ErrorMessage(statusCode, "must not be blank", "mobileNumber")
+            );
+        }
+
+        @Test
+        void shouldThrow_WhenValidationErrorsExist_UsingSpecifiedValidationGroups() {
+            var contact = ContactInfo.builder()
+                    .id(1L)
+                    .emailAddress("alice@example.org")
+                    .build();
+
+            var thrown = catchThrowable(() ->
+                    KiwiJaxrsValidations.assertValid("1", contact, KiwiValidationGroups.newObjectGroups()));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsValidationException.class);
+            var validationException = (JaxrsValidationException) thrown;
+
+            assertThat(validationException.getErrors())
+                    .extracting(ErrorMessage::getFieldName)
+                    .containsExactlyInAnyOrder("id", "mobileNumber");
+        }
+    }
+
+    @Nested
+    class AssertValidUsingPropertyPathMap {
+
+        @Test
+        void shouldNotThrow_WhenNoValidationErrors() {
+            var contact = ContactInfo.builder()
+                    .emailAddress("alice@example.org")
+                    .mobileNumber("703-555-1212")
+                    .build();
+
+            assertThatCode(() -> KiwiJaxrsValidations.assertValid(null, contact, PROPERTY_PATH_MAP))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void shouldThrow_WhenValidationErrorsExist_UsingDefaultValidationGroup() {
+            var id = "42";
+            var contact = ContactInfo.builder()
+                    .id(Long.valueOf(id))
+                    .emailAddress("")
+                    .mobileNumber("703-555-1212")
+                    .build();
+
+            var thrown = catchThrowable(() -> KiwiJaxrsValidations.assertValid(id, contact, PROPERTY_PATH_MAP));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsValidationException.class);
+            var validationException = (JaxrsValidationException) thrown;
+
+            var statusCode = 422;
+            assertThat(validationException.getStatusCode()).isEqualTo(statusCode);
+            assertThat(validationException.getOtherData()).isEmpty();
+            assertThat(validationException.getErrors()).containsExactlyInAnyOrder(
+                    new ErrorMessage(id, statusCode, "must not be blank", "Email")
+            );
+        }
+
+        @Test
+        void shouldThrow_WhenValidationErrorsExist_UsingSpecifiedValidationGroups() {
+            var contact = ContactInfo.builder()
+                    .emailAddress("alice@example.org")
+                    .build();
+
+            var thrown = catchThrowable(() ->
+                    KiwiJaxrsValidations.assertValid(null, contact, KiwiValidationGroups.existingObjectGroups()));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsValidationException.class);
+            var validationException = (JaxrsValidationException) thrown;
+
+            assertThat(validationException.getErrors())
+                    .extracting(ErrorMessage::getFieldName)
+                    .containsExactlyInAnyOrder("id", "mobileNumber");
+        }
+    }
+
+    @Nested
+    class AssertNotBlank {
+
+        @Test
+        void shouldNotThrow_WhenStringIsNotBlank() {
+            assertThatCode(() ->
+                    KiwiJaxrsValidations.assertNotBlank("emailAddress", "diane@example.org"))
+                    .doesNotThrowAnyException();
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(BlankStringArgumentsProvider.class)
+        void shouldThrow_WhenStringIsBlank(String input) {
+            var thrown = catchThrowable(() ->
+                    KiwiJaxrsValidations.assertNotBlank("mobileNumber", input));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsBadRequestException.class);
+            var badRequestException = (JaxrsBadRequestException) thrown;
+
+            var statusCode = 400;
+            assertThat(badRequestException.getStatusCode()).isEqualTo(statusCode);
+            assertThat(badRequestException.getErrors()).containsExactly(
+                    new ErrorMessage(statusCode, KiwiJaxrsValidations.MISSING_VALUE_MESSAGE, "mobileNumber")
+            );
+        }
+    }
+
+    @Nested
+    class AssertNotNull {
+
+        @Test
+        void shouldNotThrow_WhenObjectIsNotNull() {
+            assertThatCode(() ->
+                    KiwiJaxrsValidations.assertNotNull("mobileNumber", "703-555-1212"))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void shouldThrow_WhenObjectIsNull() {
+            var thrown = catchThrowable(() ->
+                    KiwiJaxrsValidations.assertNotNull("emailAddress", null));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsBadRequestException.class);
+            var badRequestException = (JaxrsBadRequestException) thrown;
+
+            var statusCode = 400;
+            assertThat(badRequestException.getStatusCode()).isEqualTo(statusCode);
+            assertThat(badRequestException.getErrors()).containsExactly(
+                    new ErrorMessage(statusCode, KiwiJaxrsValidations.MISSING_VALUE_MESSAGE, "emailAddress")
+            );
+        }
+    }
+
+    @Nested
+    class AssertTrue {
+
+        @Test
+        void shouldNotThrow_WhenValueIsTrue() {
+            assertThatCode(() ->
+                    KiwiJaxrsValidations.assertTrue(true, "It must be true"))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        void shouldThrow_WhenValueIsFalse() {
+            //noinspection ConstantConditions
+            var thrown = catchThrowable(() ->
+                    KiwiJaxrsValidations.assertTrue(false, "It must be true"));
+
+            assertThat(thrown).isExactlyInstanceOf(JaxrsBadRequestException.class);
+            var badRequestException = (JaxrsBadRequestException) thrown;
+
+            var statusCode = 400;
+            assertThat(badRequestException.getStatusCode()).isEqualTo(statusCode);
+            assertThat(badRequestException.getErrors()).containsExactly(
+                    new ErrorMessage(statusCode, "It must be true")
+            );
+        }
+    }
+
+    @Value
+    @Builder
+    private static class ContactInfo {
+
+        @Null(groups = NewObject.class)
+        @NotNull(groups = ExistingObject.class)
+        Long id;
+
+        @NotBlank
+        @Email
+        String emailAddress;
+
+        @NotBlank
+        String mobileNumber;
+    }
+}
