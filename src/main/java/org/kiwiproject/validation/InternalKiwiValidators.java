03fccccc5b8be7f96b34561c536e8292beb3b8fb
Scott Leberknight <scottleber@gmail.com>
2020-10-02 21:38:16 -0400

    Add @return javadoc tag to InternalKiwiValidators#toComparableOrNull

--- src/main/java/org/kiwiproject/validation/InternalKiwiValidators.java
+++ src/main/java/org/kiwiproject/validation/InternalKiwiValidators.java
@@ -47,6 +47,7 @@ class InternalKiwiValidators {
      * @param compareValue a string representation of a value that should be converted into the same type as the
      *                     {@code value} argument
      * @param value        the value being validated, and which defines the conversion type
+     * @return a {@link Comparable} of the same type as {@code value}
      * @implNote This is non-ideal with the massive if/else if/else, but since we have to check each type we
      * support, I cannot think of a "better" or "cleaner" way to do this without it becoming so abstract that
      * it becomes unreadable. Interestingly, neither IntelliJ not Sonar is complaining...maybe we don't have the
