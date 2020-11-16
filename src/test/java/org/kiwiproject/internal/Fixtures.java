81940d85f3af6f150aef164fe014944d389e1ed1
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-11-16 13:36:24 -0500

    Minor javadoc change

--- src/test/java/org/kiwiproject/internal/Fixtures.java
+++ src/test/java/org/kiwiproject/internal/Fixtures.java
@@ -10,8 +10,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * The Dropwizard FixtureHelpers in dropwizard-testing has a fixture method that returns a String but it does not
- * have a direct analog for the fixturePath method. Note this is in the test sources so it won't be part of an
+ * The Dropwizard {@code FixtureHelpers} in dropwizard-testing has a fixture method that returns a String but it does
+ * not have a direct analog for the fixturePath method. Note this is in the test sources so it won't be part of an
  * actual kiwi JAR. Also note that kiwi-test contains its own {@code Fixtures} class, however we cannot use that
  * here without creating a cyclic dependency between kiwi and kiwi-test (which depends on kiwi).
  */
