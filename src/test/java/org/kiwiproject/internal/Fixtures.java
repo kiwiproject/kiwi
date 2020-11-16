7674a0ab75d226f104bd111bcea357d807c083f8
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2020-11-16 13:33:19 -0500

    Replace TODO in Fixtures with a Javadoc with better explanation
    There was nothing really "todo" with this; it works and we use it
    throughout kiwi.

--- src/test/java/org/kiwiproject/internal/Fixtures.java
+++ src/test/java/org/kiwiproject/internal/Fixtures.java
@@ -9,10 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-// TODO This came in the ansible.vault.testing package (#272) but I moved to internal until figure out what to do with
-//  it. The Dropwizard FixtureHelpers in dropwizard-testing has a fixture method that returns a String but it does not
-//  have a direct analog for the fixturePath method. Currently this is in the test sources so it won't be part of an
-//  actual kiwi JAR.
+/**
+ * The Dropwizard FixtureHelpers in dropwizard-testing has a fixture method that returns a String but it does not
+ * have a direct analog for the fixturePath method. Note this is in the test sources so it won't be part of an
+ * actual kiwi JAR. Also note that kiwi-test contains its own {@code Fixtures} class, however we cannot use that
+ * here without creating a cyclic dependency between kiwi and kiwi-test (which depends on kiwi).
+ */
 public class Fixtures {
 
     public static String fixture(String resourceName) {
