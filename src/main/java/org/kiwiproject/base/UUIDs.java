e622a01fc0ac32c5202a1f7853d40591577dc02e
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2021-04-13 11:12:16 -0400

    Support case insensitive UUIDs (#544)
    Closes #536

--- src/main/java/org/kiwiproject/base/UUIDs.java
+++ src/main/java/org/kiwiproject/base/UUIDs.java
@@ -14,7 +14,7 @@ public class UUIDs {
     private static final String NIL_UUID = "00000000-0000-0000-0000-000000000000";
 
     private static final Pattern RFC4122_PATTERN =
-            Pattern.compile("^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}");
+            Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}");
 
     /**
      * Creates a new type 4 (pseudo randomly generated) UUID, and then returns it as a string.
