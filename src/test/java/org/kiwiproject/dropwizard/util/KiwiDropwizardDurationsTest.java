8be904a1c458a455a64d3bd73de28528b9858f3c
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-08-04 15:24:29 -0400

    Refactor KiwiDropwizardDurations (#765)
    * Refactor KiwiDropwizardDurations
    
    Change fromDropwizardDuration to use Java Duration's "of" factory
    method. This is better the our original implementation because we
    are making use of the units that the Dropwizard Duration contains
    and are converting using those units.
    
    * Improve test implementation to use ChronoUnit

--- src/test/java/org/kiwiproject/dropwizard/util/KiwiDropwizardDurationsTest.java
+++ src/test/java/org/kiwiproject/dropwizard/util/KiwiDropwizardDurationsTest.java
@@ -2,10 +2,16 @@ package org.kiwiproject.dropwizard.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
 import io.dropwizard.util.Duration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("KiwiDropwizardDurations")
 class KiwiDropwizardDurationsTest {
@@ -35,6 +41,29 @@ class KiwiDropwizardDurationsTest {
         void shouldConvertZeroValue() {
             assertThat(KiwiDropwizardDurations.fromDropwizardDuration(Duration.seconds(0)))
                     .isEqualTo(java.time.Duration.ofNanos(0));
+        }
+
+        @ParameterizedTest
+        @CsvSource({
+                "1 nanosecond, 1, NANOS",
+                "1000 nanoseconds, 1000, NANOS",
+                "1 microsecond, 1, MICROS",
+                "500 microseconds, 500, MICROS",
+                "1 millisecond, 1, MILLIS",
+                "42000 milliseconds, 42000, MILLIS",
+                "1 second, 1, SECONDS",
+                "420 seconds, 420, SECONDS",
+                "84 minutes, 84, MINUTES",
+                "42 hours, 42, HOURS",
+                "1 day, 1, DAYS",
+                "252 days, 252, DAYS"
+        })
+        void shouldConvertToJavaDuration(String durationSpec, long duration, ChronoUnit unit) {
+            var dwDuration = Duration.parse(durationSpec);
+
+            var expectedJavaDuration = java.time.Duration.of(duration, unit);
+
+            assertThat(KiwiDropwizardDurations.fromDropwizardDuration(dwDuration)).isEqualTo(expectedJavaDuration);
         }
     }
 }
