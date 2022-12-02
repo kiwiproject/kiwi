5a5a8f9d077a444505b5925ecd906d17111d6407
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2022-12-02 20:40:41 +0000

    Add javadoc

--- src/main/java/org/kiwiproject/base/KiwiEnums.java
+++ src/main/java/org/kiwiproject/base/KiwiEnums.java
@@ -17,11 +17,28 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class KiwiEnums {
 
+    /**
+     * Compares the given enum's {@link Enum#name() name} with the given value for equality.
+     *
+     * @param <E> the enum type
+     * @param enumValue the enum to use for the comparison
+     * @param value the value to use for the comparison, may be null
+     * @return true if the enum name equals the value, false otherwise
+     */
     public static <E extends Enum<E>> boolean equals(Enum<E> enumValue, @Nullable CharSequence value) {
         checkEnumNotNull(enumValue);
         return enumValue.name().equals(stringOrNull(value));
     }
 
+    /**
+     * Compares the given enum's {@link Enum#name() name} with the given value, ignoring
+     * case, for equality.
+     *
+     * @param <E> the enum type
+     * @param enumValue the enum to use for the comparison
+     * @param value the value to use for the comparison, may be null
+     * @return if the enum name equals the value in case-insensitive manner, false otherwise
+     */
     public static <E extends Enum<E>> boolean equalsIgnoreCase(Enum<E> enumValue, @Nullable CharSequence value) {
         checkEnumNotNull(enumValue);
         return enumValue.name().equalsIgnoreCase(stringOrNull(value));
@@ -31,22 +48,58 @@ public class KiwiEnums {
         checkArgumentNotNull(enumValue, "enumValue must not be null");
     }
 
+    /**
+     * Compares the given enum's {@link Enum#name() name} with the given value for
+     * inverse equality, i.e. they are not equal.
+     *
+     * @param <E> the enum type
+     * @param enumValue the enum to use for the comparison
+     * @param value the value to use for the comparison, may be null
+     * @return true if the enum name does not equal the value, false otherwise
+     */
     public static <E extends Enum<E>> boolean notEquals(Enum<E> enumValue, @Nullable CharSequence value) {
         return !equals(enumValue, value);
     }
 
+    /**
+     * Compares the given enum's {@link Enum#name() name} with the given value, ignoring case, for
+     * inverse equality, i.e. they are not equal.
+     *
+     * @param <E> the enum type
+     * @param enumValue the enum to use for the comparison
+     * @param value the value to use for the comparison, may be null
+     * @return true if the enum name does not equal the value in case-insensitive manner, false otherwise
+     */
     public static <E extends Enum<E>> boolean notEqualsIgnoreCase(Enum<E> enumValue, @Nullable CharSequence value) {
         return !equalsIgnoreCase(enumValue, value);
     }
 
+    /**
+     * Checks whether the given value matches the {@link Enum#name() name} of any of the given enums.
+     *
+     * @param <E> the enum type
+     * @param value the value to use for the comparison, may be null
+     * @param enumValues the enums to use for the comparison
+     * @return true if the value equals the name of any of the enums, false otherwise
+     */
     @SafeVarargs
     public static <E extends Enum<E>> boolean equalsAny(@Nullable CharSequence value, Enum<E>... enumValues) {
         checkEnumsNotNullOrEmpty(enumValues);
         return Arrays.stream(enumValues).anyMatch(enumValue -> equals(enumValue, stringOrNull(value)));
     }
 
+    /**
+     * Checks whether the given value matches the {@link Enum#name() name} of any of the given
+     * enums, ignoring case.
+     *
+     * @param <E> the enum type
+     * @param value the value to use for the comparison, may be null
+     * @param enumValues the enums to use for the comparison
+     * @return true if the value equals the name of any of the enums in case-insensitive manner, false otherwise
+     */
     @SafeVarargs
-    public static <E extends Enum<E>> boolean equalsAnyIgnoreCase(@Nullable CharSequence value, Enum<E>... enumValues) {
+    public static <E extends Enum<E>> boolean equalsAnyIgnoreCase(@Nullable CharSequence value,
+                                                                  Enum<E>... enumValues) {
         checkEnumsNotNullOrEmpty(enumValues);
         return Arrays.stream(enumValues).anyMatch(enumValue -> equalsIgnoreCase(enumValue, stringOrNull(value)));
     }
