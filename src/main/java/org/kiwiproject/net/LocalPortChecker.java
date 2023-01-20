9dac9686d9335ed99ec01dcd080bfe97e3c17621
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-01-20 11:59:24 -0500

    Add random port finder methods to LocalPortChecker (#843)
    Closes #843

--- src/main/java/org/kiwiproject/net/LocalPortChecker.java
+++ src/main/java/org/kiwiproject/net/LocalPortChecker.java
@@ -8,10 +8,11 @@ import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.util.OptionalInt;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 /**
- * Utility to check port availability.
+ * Utility to check port availability and find open ports.
  */
 @Slf4j
 public class LocalPortChecker {
@@ -27,7 +28,7 @@ public class LocalPortChecker {
      * @throws IllegalArgumentException if port is not valid
      */
     public boolean isPortAvailable(int port) {
-        checkArgument(port > 0 && port <= MAX_PORT, "Invalid port: %s", port);
+        checkArgument(isPortFromOneToMaxInclusive(port), "Invalid port: %s", port);
 
         try (var serverSocket = new ServerSocket(port); var dataSocket = new DatagramSocket(port)) {
             serverSocket.setReuseAddress(true);
@@ -50,7 +51,7 @@ public class LocalPortChecker {
      * port used to indicate a random port and is never bound directly to.
      */
     public OptionalInt findFirstOpenPortFrom(int port) {
-        checkArgument(port > 0 && port <= MAX_PORT, "Invalid start port: %s", port);
+        checkPortOneToMaxPortInclusive(port);
 
         var startPort = port - 1;
         return findFirstOpenPortAbove(startPort);
@@ -65,10 +66,68 @@ public class LocalPortChecker {
      * @throws IllegalArgumentException if port is not between 0 and ({@link #MAX_PORT} - 1)
      */
     public OptionalInt findFirstOpenPortAbove(int port) {
-        checkArgument(port >= 0 && port < MAX_PORT, "Invalid start port: %s", port);
+        checkPortZeroInclusiveToMaxPortExclusive(port);
 
         return IntStream.rangeClosed(port + 1, MAX_PORT)
                 .filter(this::isPortAvailable)
                 .findFirst();
+    }
+
+    /**
+     * Find a random open port. If no available port is found, return an empty {@link OptionalInt}.
+     *
+     * @return an optional containing the first random port found, or an empty optional
+     */
+    public OptionalInt findRandomOpenPort() {
+        return findRandomOpenPortAbove(0);
+    }
+
+    /**
+     * Find a random open port starting at the given port. If no available port is found, return an empty
+     * {@link OptionalInt}.
+     *
+     * @param port the port to start from (inclusive)
+     * @return an optional containing the first open port, or an empty optional
+     * @throws IllegalArgumentException if port is not between 1 and {@link #MAX_PORT}
+     * @implNote Since this method looks for availability of a specific port, zero is invalid because it is a reserved
+     * port used to indicate a random port and is never bound directly to.
+     */
+    public OptionalInt findRandomOpenPortFrom(int port) {
+        checkPortOneToMaxPortInclusive(port);
+
+        var startPort = port - 1;
+        return findRandomOpenPortAbove(startPort);
+    }
+
+    private static void checkPortOneToMaxPortInclusive(int port) {
+        checkArgument(isPortFromOneToMaxInclusive(port), "Invalid start port: %s", port);
+    }
+
+    private static boolean isPortFromOneToMaxInclusive(int port) {
+        return port > 0 && port <= MAX_PORT;
+    }
+
+    /**
+     * Find a random open port starting above the given port. If no available port is found, return an empty
+     * {@link OptionalInt}.
+     *
+     * @param port the port to start from (exclusive)
+     * @return an optional containing the first open port, or an empty optional
+     * @throws IllegalArgumentException if port is not between 0 and ({@link #MAX_PORT} - 1)
+     * @implNote Makes up to 100 attempts to find an open port before giving up.
+     */
+    public OptionalInt findRandomOpenPortAbove(int port) {
+        checkPortZeroInclusiveToMaxPortExclusive(port);
+
+        var minPort = port + 1;
+        var maxPortBound = MAX_PORT + 1;
+        return IntStream.generate(() -> ThreadLocalRandom.current().nextInt(minPort, maxPortBound))
+                .filter(this::isPortAvailable)
+                .limit(100)
+                .findFirst();
+    }
+
+    private static void checkPortZeroInclusiveToMaxPortExclusive(int port) {
+        checkArgument(port >= 0 && port < MAX_PORT, "Invalid start port: %s", port);
     }
 }
