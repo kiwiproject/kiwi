37f421cf682fa128518a30e0de2b98860d8ad62a
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-01-24 13:36:05 -0500

    Require release from master or main and ensure repo is up to date (#850)
    * Require release from master or main and ensure repo is up to date
    
    Closes #849

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -2,6 +2,27 @@
 # Deploy maven artifact in current directory into Maven central repository
 # using maven-release-plugin goals
 
+# check if the current branch is master or main
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "master" ] && [ "$current_branch" != "main" ]; then
+  echo "WARNING: Not on the master or main branch."
+  exit 1
+fi
+
+# fetch the latest information from the remote repository
+git fetch
+
+# check the status of the repository
+status=$(git status)
+
+# check if there are any changes
+if [[ "$status" == *"Your branch is up to date"* ]]; then
+  echo "Repository is up to date."
+else
+  echo "WARNING: Repository is not up to date. Run git pull --rebase and then re-run this script."
+  exit 1
+fi
+
 read -r -p "Really deploy to maven central repository  (yes/no)? "
 
 if [[ "$REPLY" == "yes" ]]; then
