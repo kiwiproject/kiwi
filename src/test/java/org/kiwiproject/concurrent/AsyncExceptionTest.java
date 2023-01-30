c725e8ed7a0448199e4905013f0d004b1e3884d6
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-01-30 16:01:43 -0500

    CodeQL: Fix "Unused classes and interfaces " (#891)
    Fixes #874
    Fixes #875
    Fixes #876
    Fixes #877

--- src/test/java/org/kiwiproject/concurrent/AsyncExceptionTest.java
+++ src/test/java/org/kiwiproject/concurrent/AsyncExceptionTest.java
@@ -2,10 +2,12 @@ package org.kiwiproject.concurrent;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
 
+@DisplayName("AsyncException")
 class AsyncExceptionTest {
 
     @Test
