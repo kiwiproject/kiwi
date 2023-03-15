5a1c606dead9376f71e6d7f9de07d99ea9ea3c88
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-03-15 00:33:59 +0000

    Add methods in KiwiPrimitives to parse CharSequence to int
    Part of #914

--- src/test/java/org/kiwiproject/base/KiwiPrimitivesTest.java
+++ src/test/java/org/kiwiproject/base/KiwiPrimitivesTest.java
@@ -1,10 +1,19 @@
 package org.kiwiproject.base;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+@DisplayName("KiwiPrimitives")
 @ExtendWith(SoftAssertionsExtension.class)
 class KiwiPrimitivesTest {
 
@@ -24,5 +33,73 @@ class KiwiPrimitivesTest {
         softly.assertThat(KiwiPrimitives.firstNonZero(0L, 42L)).isEqualTo(42L);
         softly.assertThatThrownBy(() -> KiwiPrimitives.firstNonZero(0L, 0L))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Nested
+    class TryParseIntOrNull {
+
+        @Test
+        void shouldReturnInteger_WhenArgumentIsParseableToInt() {
+            assertThat(KiwiPrimitives.tryParseIntOrNull("42")).isEqualTo(42);
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldReturnNull_WhenArgumentIsNullOrEmpty(String string) {
+            assertThat(KiwiPrimitives.tryParseIntOrNull(string)).isNull();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {" ", "abcd", "42a", "a42", "4,200", "420.0"})
+        void shouldReturnNull_WhenArgumentIsNotParseableToInt(String string) {
+            assertThat(KiwiPrimitives.tryParseIntOrNull(string)).isNull();
+        }
+    }
+
+    @Nested
+    class TryParseInt {
+
+        @Test
+        void shouldReturnOptionalHavingValue_WhenArgumentIsParseableToInt() {
+            assertThat(KiwiPrimitives.tryParseInt("84")).hasValue(84);
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        void shouldReturnEmptyOptional_WhenArgumentIsNulOrEmpty(String string) {
+            assertThat(KiwiPrimitives.tryParseInt(string)).isEmpty();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {" ", "abcd", "42a", "a42", "4,200", "420.0"})
+        void shouldReturnEmptyOptional_WhenArgumentIsNotParseableToInt(String string) {
+            assertThat(KiwiPrimitives.tryParseInt(string)).isEmpty();
+        }
+    }
+
+    @Nested
+    class TryParseIntOrThrow {
+
+        @Test
+        void shouldReturInt_WhenArgumentIsParseableToInt() {
+            assertThat(KiwiPrimitives.tryParseIntOrThrow("126")).isEqualTo(126);
+        }
+
+        @Test
+        void shouldThrow_WhenArgumentIsNull() {
+            assertThatIllegalStateException()
+                    .isThrownBy(() -> KiwiPrimitives.tryParseIntOrThrow(null))
+                    .withMessage("java.lang.NullPointerException")
+                    .withCauseExactlyInstanceOf(NullPointerException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"", " ", "abcd", "42a", "a42", "4,200", "420.0"})
+        void shouldThrow_WhenArgumentIsNotParseableToInt(String string) {
+            assertThatIllegalStateException()
+                    .isThrownBy(() -> KiwiPrimitives.tryParseIntOrThrow(string))
+                    .withMessageContaining(string)
+                    .withCauseExactlyInstanceOf(NumberFormatException.class);
+        }
     }
 }
