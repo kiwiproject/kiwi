5882aae280870f1e8dd287876140170bac2ecdbf
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-03-15 09:12:13 -0400

    Add methods in KiwiPrimitives to parse CharSequence to int (#915)
    Part of #914

--- src/main/java/org/kiwiproject/base/KiwiPrimitives.java
+++ src/main/java/org/kiwiproject/base/KiwiPrimitives.java
@@ -4,6 +4,8 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.OptionalInt;
+
 /**
  * Static utilities that operate on primitive values, and are not already provided by classes in Guava's
  * {@link com.google.common.primitives} package.
@@ -40,5 +42,48 @@ public class KiwiPrimitives {
     private static long checkNonZero(long value) {
         checkArgument(value != 0, "One of the arguments must be non-zero");
         return value;
+    }
+
+    /**
+     * Attempt to parse the given {@link CharSequence} to an {@link Integer}.
+     *
+     * @param cs the value to parse
+     * @return the value as an Integer or {@code null} if the value cannot be parsed
+     */
+    public static Integer tryParseIntOrNull(CharSequence cs) {
+        try {
+            return Integer.valueOf(cs.toString());
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Attempt to parse the given {@link CharSequence} to an {@code int}.
+     *
+     * @param cs the value to parse
+     * @return an {@link OptionalInt} that will contain the parsed value or will be empty if the input cannot be parsed
+     */
+    public static OptionalInt tryParseInt(CharSequence cs) {
+        try {
+            return OptionalInt.of(Integer.parseInt(cs.toString()));
+        } catch (Exception e) {
+            return OptionalInt.empty();
+        }
+    }
+
+    /**
+     * Attempt to parse the given {@link CharSequence} to an {@code int}.
+     *
+     * @param cs the value to parse
+     * @return the parsed {@code int} value if successful; if it cannot be parsed this method always throws an exception
+     * @throws IllegalStateException if the value cannot be parsed
+     */
+    public static int tryParseIntOrThrow(CharSequence cs) {
+        try {
+            return Integer.parseInt(cs.toString());
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
