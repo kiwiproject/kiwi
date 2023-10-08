3674f23a7353f3dc88ea20c07b14ac2da4a290b8
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-10-08 12:44:23 -0400

    Add more tests to PropertyMaskingOptionsTest
    * Ensure allows custom values (it should but...)
    * Add test to verify the default maskedFieldRegexps is a mutable List

--- src/test/java/org/kiwiproject/json/PropertyMaskingOptionsTest.java
+++ src/test/java/org/kiwiproject/json/PropertyMaskingOptionsTest.java
@@ -2,9 +2,12 @@ package org.kiwiproject.json;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 @DisplayName("PropertyMaskingOptions")
 class PropertyMaskingOptionsTest {
@@ -25,5 +28,30 @@ class PropertyMaskingOptionsTest {
         assertThat(defaultOptions.getMaskedFieldRegexps()).isEmpty();
         assertThat(defaultOptions.getMaskedFieldReplacementText()).isEqualTo("********");
         assertThat(defaultOptions.getSerializationErrorReplacementText()).isEqualTo("(unable to serialize field)");
+    }
+
+    @Test
+    void shouldNotMakeMaskedFieldRegexpsUnmodifiableByDefault() {
+        var options = PropertyMaskingOptions.builder().build();
+
+        options.getMaskedFieldRegexps().add(".*password.*");
+
+        assertThat(options.getMaskedFieldRegexps()).containsExactly(".*password.*");
+    }
+
+    @Test
+    void shouldAllowCustomValues() {
+        var options = PropertyMaskingOptions.builder()
+                .maskedFieldRegexps(List.of(".*password.*"))
+                .maskedFieldReplacementText("-----")
+                .serializationErrorReplacementText("(error serializing field)")
+                .build();
+
+        assertAll(
+                () -> assertThat(options.getMaskedFieldRegexps()).containsExactly(".*password.*"),
+                () -> assertThat(options.getMaskedFieldRegexps()).isUnmodifiable(),
+                () -> assertThat(options.getMaskedFieldReplacementText()).isEqualTo("-----"),
+                () -> assertThat(options.getSerializationErrorReplacementText()).isEqualTo("(error serializing field)")
+        );
     }
 }
