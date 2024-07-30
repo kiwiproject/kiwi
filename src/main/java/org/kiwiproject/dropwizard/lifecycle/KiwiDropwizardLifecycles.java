ff30117162c3dbee672b1536459ba2d148437bf4
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-07-30 12:54:47 -0400

    Add new lifecycle management methods in KiwiDropwizardLifecycles (#1175)
    Add methods in KiwiDropwizardLifecycles to manage and only provide a
    start or a stop action
    
    Closes  #1163

--- src/main/java/org/kiwiproject/dropwizard/lifecycle/KiwiDropwizardLifecycles.java
+++ src/main/java/org/kiwiproject/dropwizard/lifecycle/KiwiDropwizardLifecycles.java
@@ -1,5 +1,6 @@
 package org.kiwiproject.dropwizard.lifecycle;
 
+import com.google.common.util.concurrent.Runnables;
 import io.dropwizard.lifecycle.Managed;
 import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
 import lombok.experimental.UtilityClass;
@@ -15,8 +16,8 @@ public class KiwiDropwizardLifecycles {
      * is {@code stopAction}, and attaches it to the given Dropwizard {@code lifecycle}.
      * <p>
      * Useful when you have some external object that has start and stop methods, but you don't want to clutter your
-     * code by creating an anonymous inner class just to specify the start and stop actions. For example if you have an
-     * ActiveMQ {@code PooledConnectionFactory} (which has {@code start} and {@code stop} methods) you can simply
+     * code by creating an anonymous inner class just to specify the start and stop actions. For example, if you have
+     * an ActiveMQ {@code PooledConnectionFactory} (which has {@code start} and {@code stop} methods) you can simply
      * call this method:
      * <p>
      * {@code KiwiDropwizardLifecycles.manage(lifecycle, () -> factory.start(), () -> factory.stop());}
@@ -41,5 +42,49 @@ public class KiwiDropwizardLifecycles {
                 stopAction.run();
             }
         });
+    }
+
+    /**
+     * Creates a Dropwizard {@link Managed} whose start action is {@code startAction},
+     * and attaches it to the given Dropwizard {@code lifecycle}.
+     * <p>
+     * Useful when you have some external object that has a start method, but you don't want to clutter your
+     * code by creating an anonymous inner class just to specify the start action. For example, if you have
+     * a monitoring class that needs to start when the application starts, you can ensure that happens
+     * using code like:
+     * <p>
+     * {@code KiwiDropwizardLifecycles.manage(lifecycle, () -> monitor.start());}
+     * <p>
+     * To make the code cleaner, use method references:
+     * <p>
+     * {@code KiwiDropwizardLifecycles.manage(lifecycle, monitor::start);}
+     *
+     * @param lifecycle   the lifecycle to manage
+     * @param startAction the action to run when Dropwizard starts the application
+     */
+    public static void manageOnlyStart(LifecycleEnvironment lifecycle, Runnable startAction) {
+        manage(lifecycle, startAction, Runnables.doNothing());
+    }
+
+    /**
+     * Creates a Dropwizard {@link Managed} whose stop action is {@code stopAction},
+     * and attaches it to the given Dropwizard {@code lifecycle}.
+     * <p>
+     * Useful when you have some external object that has a stop method, but you don't want to clutter your
+     * code by creating an anonymous inner class just to specify the stop action. For example, if you have
+     * an HTTP {@code Client} class that needs to stop when the application shuts down to ensure resources
+     * are properly closed, you can ensure that happens using code like:
+     * <p>
+     * {@code KiwiDropwizardLifecycles.manage(lifecycle, () -> client.close());}
+     * <p>
+     * To make the code cleaner, use method references:
+     * <p>
+     * {@code KiwiDropwizardLifecycles.manage(lifecycle, client::close);}
+     *
+     * @param lifecycle   the lifecycle to manage
+     * @param stopAction  the action to run when Dropwizard stops the application
+     */
+    public static void manageOnlyStop(LifecycleEnvironment lifecycle, Runnable stopAction) {
+        manage(lifecycle, Runnables.doNothing(), stopAction);
     }
 }
