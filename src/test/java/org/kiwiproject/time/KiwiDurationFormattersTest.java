cc0ca223ab435b64a7c15249abe02e4bf0423638
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-06-29 14:48:47 -0400

    Enhancements to KiwiDurationFormatters
    * Handle durations that are less than one second.
    * Add a method to format down to nanosecond durations.
    * Change reporting of zero durations to be consistent based on units.
    * Update methods that accept Java and Dropwizard Duration to call
      the new nanosecond method.
    
    Closes #1290
    Closes #1291

--- src/test/java/org/kiwiproject/time/KiwiDurationFormattersTest.java
+++ src/test/java/org/kiwiproject/time/KiwiDurationFormattersTest.java
@@ -1,17 +1,22 @@
 package org.kiwiproject.time;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @DisplayName("KiwiDurationFormatters")
 class KiwiDurationFormattersTest {
 
     private static final String SEVEN_HOURS_TWENTY_FIVE_MINUTES = "7 hours 25 minutes";
+    private static final String ONE_SECOND = "1 second";
 
     @SuppressWarnings("removal")
     @Nested
@@ -62,6 +67,41 @@ class KiwiDurationFormattersTest {
             var words = KiwiDurationFormatters.formatJavaDurationWords(null);
             assertThat(words).isEqualTo("null");
         }
+
+        @ParameterizedTest
+        @ValueSource(longs = {-500, -10, -1})
+        void shouldRequireNonNegativeDuration(long seconds) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> KiwiDurationFormatters.formatJavaDurationWords(Duration.ofSeconds(seconds)))
+                    .withMessage("duration must not be negative");
+        }
+
+        @Test
+        void shouldFormatZeroDuration() {
+            assertThat(KiwiDurationFormatters.formatJavaDurationWords(Duration.ZERO)).isEqualTo("0 seconds");
+        }
+
+        @Test
+        void shouldFormatOneSecondDuration() {
+            assertThat(KiwiDurationFormatters.formatJavaDurationWords(Duration.ofMillis(1000)))
+                    .isEqualTo(ONE_SECOND);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = {1, 2, 50, 500, 999})
+        void shouldFormatMillisecondDurations(long milliseconds) {
+            var units = milliseconds == 1 ? " millisecond" : " milliseconds";
+            assertThat(KiwiDurationFormatters.formatJavaDurationWords(Duration.ofMillis(milliseconds)))
+                    .isEqualTo(milliseconds + units);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { 1, 2, 25_000, 50_000, 500_000, 999_999 })
+        void shouldFormatNanosecondDurations(long nanoseconds) {
+            var units = nanoseconds == 1 ? " nanosecond" : " nanoseconds";
+            assertThat(KiwiDurationFormatters.formatJavaDurationWords(Duration.ofNanos(nanoseconds)))
+                    .isEqualTo(nanoseconds + units);
+        }
     }
 
     @Nested
@@ -78,15 +118,126 @@ class KiwiDurationFormattersTest {
             var words = KiwiDurationFormatters.formatDropwizardDurationWords(null);
             assertThat(words).isEqualTo("null");
         }
+
+        @ParameterizedTest
+        @ValueSource(longs = { -500, -10, -1 })
+        void shouldRequireNonNegativeDuration(long seconds) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> KiwiDurationFormatters.formatDropwizardDurationWords(io.dropwizard.util.Duration.seconds(seconds)))
+                    .withMessage("duration must not be negative");
+        }
+
+        @Test
+        void shouldFormatZeroDuration() {
+            assertThat(KiwiDurationFormatters.formatDropwizardDurationWords(io.dropwizard.util.Duration.minutes(0)))
+                    .isEqualTo("0 seconds");
+        }
+
+        @Test
+        void shouldFormatOneSecondDuration() {
+            assertThat(KiwiDurationFormatters.formatDropwizardDurationWords(io.dropwizard.util.Duration.milliseconds(1000)))
+                    .isEqualTo(ONE_SECOND);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { 1, 2, 50, 500, 999 })
+        void shouldFormatMillisecondDurations(long milliseconds) {
+            var units = milliseconds == 1 ? " millisecond" : " milliseconds";
+            assertThat(KiwiDurationFormatters.formatDropwizardDurationWords(io.dropwizard.util.Duration.milliseconds(milliseconds)))
+                    .isEqualTo(milliseconds + units);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { 1, 2, 25_000, 50_000, 500_000, 999_999 })
+        void shouldFormatNanosecondDurations(long nanoseconds) {
+            var units = nanoseconds == 1 ? " nanosecond" : " nanoseconds";
+            assertThat(KiwiDurationFormatters.formatDropwizardDurationWords(io.dropwizard.util.Duration.nanoseconds(nanoseconds)))
+                    .isEqualTo(nanoseconds + units);
+        }
     }
 
     @Nested
-    class FormatMillisecondsDurationWords {
+    class FormatMillisecondDurationWords {
 
         @Test
         void shouldFormatMilliseconds() {
             var words = KiwiDurationFormatters.formatMillisecondDurationWords(Duration.ofMinutes(445).toMillis());
             assertThat(words).isEqualTo(SEVEN_HOURS_TWENTY_FIVE_MINUTES);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { -500, -10, -1 })
+        void shouldRequireNonNegativeDuration(long milliseconds) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> KiwiDurationFormatters.formatMillisecondDurationWords(milliseconds))
+                    .withMessage("duration must not be negative");
+        }
+
+        @Test
+        void shouldFormatZeroDuration() {
+            assertThat(KiwiDurationFormatters.formatMillisecondDurationWords(0)).isEqualTo("0 milliseconds");
+        }
+
+        @Test
+        void shouldFormatOneSecondDuration() {
+            assertThat(KiwiDurationFormatters.formatMillisecondDurationWords(1000))
+                    .isEqualTo(ONE_SECOND);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { 1, 2, 50, 500, 999 })
+        void shouldFormatMillisecondDurations(long milliseconds) {
+            var units = milliseconds == 1 ? " millisecond" : " milliseconds";
+            assertThat(KiwiDurationFormatters.formatMillisecondDurationWords(milliseconds))
+                    .isEqualTo(milliseconds + units);
+        }
+    }
+
+    @Nested
+    class FormatNanosecondDurationWords {
+
+        @Test
+        void shouldFormatNanoseconds() {
+            var words = KiwiDurationFormatters.formatNanosecondDurationWords(Duration.ofMinutes(445).toNanos());
+            assertThat(words).isEqualTo(SEVEN_HOURS_TWENTY_FIVE_MINUTES);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { -500, -10, -1 })
+        void shouldRequireNonNegativeDuration(long nanoseconds) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> KiwiDurationFormatters.formatMillisecondDurationWords(nanoseconds))
+                    .withMessage("duration must not be negative");
+        }
+
+        @Test
+        void shouldFormatZeroDuration() {
+            assertThat(KiwiDurationFormatters.formatNanosecondDurationWords(0)).isEqualTo("0 nanoseconds");
+        }
+
+        @Test
+        void shouldFormatOneSecondDuration() {
+            assertThat(KiwiDurationFormatters.formatNanosecondDurationWords(TimeUnit.SECONDS.toNanos(1)))
+                    .isEqualTo(ONE_SECOND);
+        }
+
+
+        @ParameterizedTest
+        @ValueSource(longs = { 1, 2, 50, 500, 999 })
+        void shouldFormatMillisecondDurations(long milliseconds) {
+            var nanos = TimeUnit.MILLISECONDS.toNanos(milliseconds);
+            var units = milliseconds == 1 ? " millisecond" : " milliseconds";
+            assertThat(KiwiDurationFormatters.formatNanosecondDurationWords(nanos))
+                    .isEqualTo(milliseconds + units);
+        }
+
+
+        @ParameterizedTest
+        @ValueSource(longs = { 1, 2, 25_000, 50_000, 500_000, 999_999 })
+        void shouldFormatNanosecondDurations(long nanoseconds) {
+            var units = nanoseconds == 1 ? " nanosecond" : " nanoseconds";
+            assertThat(KiwiDurationFormatters.formatNanosecondDurationWords(nanoseconds))
+                    .isEqualTo(nanoseconds + units);
         }
     }
 }
