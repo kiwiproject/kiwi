44f35f0474f2693e220d061bb0203ad641d58a01
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-08-05 16:53:02 -0400

    Add getIfPresentFuzzy method to KiwiEnums (#1308)
    Closes  #1301

--- src/main/java/org/kiwiproject/base/KiwiEnums.java
+++ src/main/java/org/kiwiproject/base/KiwiEnums.java
@@ -2,9 +2,11 @@ package org.kiwiproject.base;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.kiwiproject.base.KiwiPreconditions.checkArgumentNotNull;
 
+import com.google.common.base.CaseFormat;
 import com.google.common.base.Enums;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.EnumUtils;
@@ -15,6 +17,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
@@ -23,6 +27,13 @@ import java.util.stream.Stream;
 @UtilityClass
 public class KiwiEnums {
 
+    private static final Pattern WHITESPACE = Pattern.compile("\\s+");
+    private static final Pattern MULTIPLE_UNDERSCORES = Pattern.compile("_+");
+    @SuppressWarnings("java:S5998")
+    private static final Pattern LOWER_CAMEL = Pattern.compile("^[a-z][a-z0-9]*([A-Z][a-z0-9]*)*$");
+    @SuppressWarnings("java:S5998")
+    private static final Pattern UPPER_CAMEL = Pattern.compile("^[A-Z][a-z0-9]*(?:[A-Z][a-z0-9]*)*$");
+    
     /**
      * Returns a list of the constants in the specified enum type.
      *
@@ -89,6 +100,121 @@ public class KiwiEnums {
         // First try the value as-is, then fallback to the value with leading and trailing whitespace stripped
         return Optional.ofNullable(EnumUtils.getEnumIgnoreCase(enumClass, value))
             .or(() -> Optional.ofNullable(EnumUtils.getEnumIgnoreCase(enumClass, value.strip())));
+    }
+
+    /**
+     * Return an optional enum constant for the given enum type and value, compared against the enum constants
+     * in a fuzzy manner.
+     * <p>
+     * This method uses the following algorithm:
+     * <ol>
+     * <li>
+     *   Check for a case-insensitive match against the enum constant {@code name()}. Return if match.
+     * </li>
+     * <li>
+     *    Strip leading and trailing whitespace from the input.
+     * </li>
+     * <li>
+     *   Replace internal white space, dashes, and periods with underscores
+     *   (e.g., {@code "pending payment"} becomes {@code "pending_payment"}).
+     * </li>
+     * <li>
+     *   Collapse multiple consecutive underscores into a single underscore.
+     * </li>
+     * <li>
+     *   Check for a case-insensitive match of the normalized text against {@code name()}. Return if match.
+     * </li>
+     * <li>
+     *   Detect if the normalized input is in {@code lowerCamel} or {@code upperCamel} format.
+     *   If so, convert it to {@code UPPER_UNDERSCORE} and check for a case-insensitive match of the
+     *   converted {@code camelCase} text against {@code name()}. Return if match.
+     * </li>
+     * <li>
+     *   Check for a case-insensitive match of the original input against {@code toString()}. Return if match.
+     * </li>
+     * <li>
+     *   Return an empty {@code Optional} since there is no match.
+     * </li>
+     * </ol>
+     * 
+     * @param <E> the enum type
+     * @param enumClass the enum class
+     * @param value the string value to compare against; may be blank
+     * @return an Optional that may contain an {@link Enum} constant or be empty
+     * @implNote This is inspired by and adapted from Dropwizard's {@code Enums#fromStringFuzzy} method.
+     * It is even more permissive than the Dropwizard method.
+     */
+    public static <E extends Enum<E>> Optional<E> getIfPresentFuzzy(Class<E> enumClass, @Nullable String value) {
+        checkEnumClass(enumClass);
+        
+        if (isBlank(value)) {
+            return Optional.empty();
+        }
+
+         E[] constants = enumClass.getEnumConstants();
+
+        // Check for exact matches (case-insensitive) before modifying the input
+        var exactMatch = matchOrNull(constants, Enum::name, value);
+        if (nonNull(exactMatch)) {
+            return Optional.of(exactMatch);
+        }
+
+        // Replace internal whitespace, dashes, and periods with underscores
+        var text = normalizeInput(value);
+
+        // Check for matches on enum name() against text
+        var nameMatch = matchOrNull(constants, Enum::name, text);
+        if (nonNull(nameMatch)) {
+            return Optional.of(nameMatch);
+        }
+
+        // Check if the text is in lowerCamel or upperCamel format. There is no need
+        // to check for lower-hyphen since dashes (hyphens) were already changed to
+        // underscores. If the format is one of these, convert to UPPER_UNDERSCORE,
+        // then check for matches.
+        var camelCaseFormat = detectCamelCaseFormatOrNull(text);
+
+        if (nonNull(camelCaseFormat)) {
+            var convertedCamel = camelCaseFormat
+                    .converterTo(CaseFormat.UPPER_UNDERSCORE)
+                    .convert(text);
+        
+            // Check for matches on enum name() against camelText
+            var convertedCamelMatch = matchOrNull(constants, Enum::name, convertedCamel);
+            if (nonNull(convertedCamelMatch)) {
+                return Optional.of(convertedCamelMatch);
+            }
+        }
+
+        // Last, check against enum toString() using the original input value
+        var toStringMatch = matchOrNull(constants, Enum::toString, value);
+
+        return Optional.ofNullable(toStringMatch);
+    }
+
+    private static String normalizeInput(String input) {
+        var modified = WHITESPACE.matcher(input.strip()).replaceAll("_")
+                .replace('-', '_')
+                .replace('.', '_');
+        return MULTIPLE_UNDERSCORES.matcher(modified).replaceAll("_");
+    }
+
+    @Nullable
+    private static <E extends Enum<E>> E matchOrNull(E[] constants, Function<E, String> accessor, String candidate) {
+        return Arrays.stream(constants)
+                .filter(constant -> accessor.apply(constant).equalsIgnoreCase(candidate))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Nullable
+    static CaseFormat detectCamelCaseFormatOrNull(String input) {
+        if (LOWER_CAMEL.matcher(input).matches()) {
+            return CaseFormat.LOWER_CAMEL;
+        } else if (UPPER_CAMEL.matcher(input).matches()) {
+            return CaseFormat.UPPER_CAMEL;
+        }
+        return null;
     }
 
     private static <E extends Enum<E>> void checkEnumClass(Class<E> enumClass) {
