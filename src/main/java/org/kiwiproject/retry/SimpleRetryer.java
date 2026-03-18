ae9a0f437709e4c4597de27f3013ca0656707c6a
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-03-18 21:28:22 +0000

    fix: tryGetObjectCollectingErrors ignores configured log level
    Fixes #1381

--- src/main/java/org/kiwiproject/retry/SimpleRetryer.java
+++ src/main/java/org/kiwiproject/retry/SimpleRetryer.java
@@ -207,6 +207,7 @@ public class SimpleRetryer {
                 retryDelayTime, retryDelayUnit,
                 environment,
                 type,
+                logLevelForSubsequentAttempts,
                 supplier
         );
     }
