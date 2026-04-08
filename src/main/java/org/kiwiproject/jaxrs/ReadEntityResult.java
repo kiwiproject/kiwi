b8f39a6b99881545bb5efcbf33fa6ad8aeae5558
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-04-07 21:28:20 -0400

    docs: improve Javadoc in ReadEntityResult

--- src/main/java/org/kiwiproject/jaxrs/ReadEntityResult.java
+++ src/main/java/org/kiwiproject/jaxrs/ReadEntityResult.java
@@ -10,14 +10,15 @@ import static java.util.Objects.nonNull;
  * <p>
  * At most one of {@code entity} or {@code exception} will be non-null at any given time.
  * If the read succeeded, {@code exception} will be null, though {@code entity} may also
- * be null if the response had no body (e.g. a 204 No Content response).
+ * be null if the response did not have a body (e.g., a 204 No Content response).
  * If the read failed, {@code exception} will be non-null and {@code entity} will be null.
  * <p>
  * Use {@link #hasEntity()} or {@link #hasException()} to determine the outcome
  * before accessing the value.
  *
  * @param <T>       the type of the entity
- * @param entity    the entity read from the response, or null if the response had no body or an exception occurred
+ * @param entity    the entity read from the response, or null if the response did not have a body
+ *                  or an exception occurred
  * @param exception the exception that occurred while reading the entity, or null if successful
  */
 public record ReadEntityResult<T>(T entity, Exception exception) {
@@ -29,14 +30,14 @@ public record ReadEntityResult<T>(T entity, Exception exception) {
      * @throws IllegalArgumentException if both entity and exception are non-null
      */
     public ReadEntityResult {
-         checkArgument(!(nonNull(entity) && nonNull(exception)),
-            "entity and exception cannot both be non-null");
+        checkArgument(!(nonNull(entity) && nonNull(exception)),
+                "entity and exception cannot both be non-null");
     }
 
     /**
      * Returns true if the read succeeded, i.e., no exception occurred.
-     * Note that the entity itself may still be null if the response had no body,
-     * such as a 204 No Content response.
+     * Note that the entity itself may still be null if the response did not
+     * have a body, such as a 204 No Content response.
      *
      * @return true if no exception occurred, false otherwise
      */
